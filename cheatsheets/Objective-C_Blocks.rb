073863a24232d2f27533fc70aeb9101d07f8618f
Bogdan Popescu <bogdansrc@gmail.com>
2014-02-20 15:28:48 +0200

    Objective-C Blocks

---
+++ cheatsheets/Objective-C_Blocks.rb
@@ -0,0 +1,42 @@
+cheatsheet do
+  title 'Objective-C Blocks'
+  docset_file_name 'Objective-C_Blocks'
+  keyword 'objc'
+  platform 'appledoc'
+  source_url 'https://github.com/Kapeli/cheatsheets#readme'
+
+  category do
+    id 'Block Syntax'
+    entry do
+        name 'Block as a local variable'
+        notes <<-'END'
+        <pre class="highlight objective_c"><span class="kt">returnType</span> (^<span class="nf">blockName</span>)(<span class="mi">parameterTypes</span>) = ^<span class="kt">returnType</span>(<span class="mi">parameters</span>) {...};</pre>
+        END
+    end
+    entry do
+        name 'Block as a property'
+        notes <<-'END'
+        <pre class="highlight objective_c">@property (nonatomic, copy) <span class="kt">returnType</span> (^<span class="nf">blockName</span>)(<span class="mi">parameterTypes</span>);</pre>
+        END
+    end
+    entry do
+        name 'Block as a method parameter'
+        notes <<-'END'
+        <pre class="highlight objective_c">- (void)someMethodThatTakesABlock:(<span class="kt">returnType</span> (^)(<span class="mi">parameterTypes</span>))<span class="nf">blockName</span>;</pre>
+        END
+    end
+    entry do
+        name 'Block as an argument to a method call'
+        notes <<-'END'
+        <pre class="highlight objective_c">[someObject someMethodThatTakesABlock: ^<span class="kt">returnType</span> (<span class="mi">parameters</span>) {...}];</pre>
+        END
+    end
+    entry do
+        name 'Block as typedef'
+        notes <<-'END'
+        <pre class="highlight objective_c">typedef <span class="kt">returnType</span> (^<span class="nf">TypeName</span>)(<span class="mi">parameterTypes</span>);
+        <span class="nf">TypeName</span> blockName = ^returnType(<span class="mi">parameters</span>) {...};</pre>
+        END
+    end
+  end
+end
