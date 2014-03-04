d600f985f137496cf6de9e98d143ee92a07753b9
Kilian Haack <haack@silpion.de>
2014-03-04 20:27:30 +0100

    added Xcode Shortcuts

---
+++ cheatsheets/Xcode_Shortcuts.rb
@@ -0,0 +1,315 @@
+cheatsheet do
+  title 'Xcode Shortcuts'
+  docset_file_name 'Xcode_Shortcuts'
+  keyword 'xcode'
+  source_url 'http://cheat.kapeli.com'
+
+
+  category do
+    id 'Navigation (Editor)'
+    entry do
+      command 'CMD+SHIFT+O'
+      name 'Open Quickly'
+    end
+    entry do
+      command 'CMD+J'
+      name 'Move Focus to Editor'
+    end
+    entry do
+      command 'CMD+CTRL+Arrow Up'
+      name 'Next Counterpart'
+    end
+    entry do
+      command 'CMD+CTRL+Arrow Down'
+      name 'Previous Counterpart'
+    end
+    entry do
+      command 'CMD+CTRL+Arrow Left'
+      name 'Previous recent File'
+    end
+    entry do
+      command 'CMD+CTRL+Arrow Right'
+      name 'Next recent File'
+    end
+    entry do
+      command 'CMD+Click'
+      name 'Definition of Symbol'
+    end
+    entry do
+      command 'CMD+L'
+      name 'Go to Line'
+    end
+    entry do
+      command 'CMD+ALT+Arrow Left'
+      name 'Fold Section'
+    end
+    entry do
+      command 'CMD+CTRL+Arrow Right'
+      name 'Unfold Section'
+    end
+  end
+
+
+  category do
+    id 'Editing'
+    entry do
+      command 'CMD+ALT+Return'
+      name 'Show Assistant Editor'
+    end
+    entry do
+      command 'CMD+Return'
+      name 'Hide Assistant Editor'
+    end
+    entry do
+      command 'CTRL+Space'
+      name 'Toggle Completion'
+    end
+    entry do
+      command 'CMD+CTRL+E'
+      name 'Edit all in Scope'
+    end
+    entry do
+      command 'CMD+]'
+      name 'Indent Selection'
+    end
+    entry do
+      command 'CMD+['
+      name 'Outdent Selection'
+    end
+    entry do
+      command 'CTRL+I'
+      name 'Fix Indentation'
+    end
+    entry do
+      command 'CMD+/'
+      name 'Comment / Uncomment'
+    end
+    entry do
+      command 'CMD+ALT+['
+      name 'Move Line up'
+    end
+    entry do
+      command 'CMD+ALT+]'
+      name 'Move Line down'
+    end
+  end
+
+
+  category do
+    id 'Search'
+    entry do
+      command 'CMD+F'
+      name 'Find in File'
+    end
+    entry do
+      command 'CMD+ALT+F'
+      name 'Find & Replace in File'
+    end
+    entry do
+      command 'CMD+SHIFT+F'
+      name 'Find in Project'
+    end
+    entry do
+      command 'CMD+ALT+SHIFT+F'
+      name 'Find & Replace in Project'
+    end
+  end
+
+
+  category do
+    id 'Tabs'
+    entry do
+      command 'CMD+T'
+      name 'New Tab'
+    end
+    entry do
+      command 'CMD+}'
+      name 'Previous Tab'
+    end
+    entry do
+      command 'CMD+{'
+      name 'Next Tab'
+    end
+  end
+
+
+  category do
+    id 'Navigator'
+    entry do
+      command 'CMD+0'
+      name 'Show / Hide Navigator'
+    end
+    entry do
+      command 'CMD+1'
+      name 'Project Navigator'
+    end
+    entry do
+      command 'CMD+2'
+      name 'Symbol Navigator'
+    end
+    entry do
+      command 'CMD+3'
+      name 'Find Navigator'
+    end
+    entry do
+      command 'CMD+4'
+      name 'Issue Navigator'
+    end
+    entry do
+      command 'CMD+5'
+      name 'Test Navigator'
+    end
+    entry do
+      command 'CMD+6'
+      name 'Debug Navigator'
+    end
+    entry do
+      command 'CMD+7'
+      name 'Breakpoint Navigator'
+    end
+    entry do
+      command 'CMD+8'
+      name 'Log Navigator'
+    end
+    entry do
+      command 'ALT+Click'
+      name 'Open in Assistant Navigator'
+    end
+    entry do
+      command 'SHIFT+ALT+Click'
+      name 'decide where to open'
+    end
+  end
+
+
+  category do
+    id 'Debugging'
+    entry do
+      command 'CMD+SHIFT+Y'
+      name 'Toggle Debug Area'
+    end
+    entry do
+      command 'CMD+\''
+      name 'Next Issue'
+    end
+    entry do
+      command 'CMD+"'
+      name 'Previous Issue'
+    end
+    entry do
+      command 'CMD+\ '
+      name 'Add Breakpoint'
+    end
+    entry do
+      command 'CMD+Y'
+      name 'Toggle Breakpoints'
+    end
+    entry do
+      command 'CMD+\''
+      name 'Next Issue'
+    end
+    entry do
+      command 'CMD+K'
+      name 'Clear Console'
+    end
+  end
+
+
+  category do
+    id 'Documentation'
+    entry do
+      command 'CMD+ALT+? '
+      name 'Show Documentation'
+    end
+    entry do
+      command 'CMD+ALT+CTRL+\ '
+      name 'Show Documentation for Selection'
+    end
+    entry do
+      command 'ALT+Click '
+      name 'Show Help for clicked Symbol'
+    end
+    entry do
+      command 'ALT+DoubleClick '
+      name 'Show Documentation for clicked Symbol'
+    end
+  end
+
+
+  category do
+    id 'Build & Run'
+    entry do
+      command 'CMD+B '
+      name 'Build'
+    end
+    entry do
+      command 'CMD+R '
+      name 'R'
+    end
+    entry do
+      command 'CMD+. '
+      name 'Stop'
+    end
+    entry do
+      command 'CMD+SHIFT+B '
+      name 'Analyze'
+    end
+    entry do
+      command 'CMD+U'
+      name 'Test'
+    end
+    entry do
+      command 'CMD+SHIFT+K'
+      name 'Build'
+    end
+  end
+
+
+  category do
+    id 'Utilities'
+    entry do
+      command 'CMD+ALT+0 '
+      name 'Show/Hide'
+    end
+    entry do
+      command 'CMD+ALT+1'
+      name 'File Inspector'
+    end
+    entry do
+      command 'CMD+ALT+2'
+      name 'Quick Help Inspector'
+    end
+    entry do
+      command 'CMD+ALT+3'
+      name 'Identity Inspector'
+    end
+    entry do
+      command 'CMD+ALT+4'
+      name 'Attributes Inspector'
+    end
+    entry do
+      command 'CMD+ALT+5'
+      name 'Size Inspector'
+    end
+    entry do
+      command 'CMD+ALT+6'
+      name 'Connections Inspector'
+    end
+    entry do
+      command 'CTRL+ALT+CMD+1'
+      name 'File Template Library'
+    end
+    entry do
+      command 'CTRL+ALT+CMD+2'
+      name 'Code Snippet Library'
+    end
+    entry do
+      command 'CTRL+ALT+CMD+3'
+      name 'Object Library'
+    end
+    entry do
+      command 'CTRL+ALT+CMD+4'
+      name 'Media Library'
+    end
+  end
+end
