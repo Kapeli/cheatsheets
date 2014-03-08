5168a65b7d2dbe20fef59f2d61e68249d4dba64a
Rob Simpson <rsimpson@lits-inc.com>
2014-03-08 18:15:05 -0500

    Add of Sublime Text 3 cheatsheet

---
+++ cheatsheets/Sublime_Text_3.rb
@@ -0,0 +1,860 @@
+cheatsheet do
+	title 'Sublime Text 3 (OSX)'
+	docset_file_name 'Sublime_Text_3'
+	keyword 'sublime text'
+	source_url ''
+
+	category do
+		id 'General'
+
+		entry do
+			name 'Command Palette'
+			notes "
+				'''
+				⌘ + ⇧ + P
+				```"
+		end
+
+		entry do
+			name 'Goto Anything'
+			notes "
+				```
+				⌘ + P
+				```
+			"
+		end
+
+		entry do
+			name 'Goto Symbol'
+			notes "
+				```
+				⌘ + R
+				```
+			"
+		end
+
+		entry do
+			name 'New File'
+			notes "
+				```
+				⌘ + N
+				```
+			"
+		end
+
+		entry do
+			name 'Save File'
+			notes "
+				```
+				⌘ + S
+				```
+			"
+		end
+
+		entry do
+			name 'Save File As'
+			notes "
+				```
+				⌘ + ⇧ + S
+				```"
+		end
+
+		entry do
+			name 'Save All'
+			notes "
+				```
+				⌘ + ⌥ + S
+				```"
+		end
+
+		entry do
+			name 'Open File'
+			notes "
+				```
+				⌘ + O
+				```"
+		end
+
+		entry do
+			name 'Close File'
+			notes "
+				```
+				⌘ + W
+				```"
+		end
+
+		entry do
+			name 'Reopen Closed File'
+			notes "
+				```
+				⌘ + ⇧ + T
+				```"
+		end
+
+		entry do
+			name 'New Window'
+			notes "
+				```
+				⌘ + ⇧ + N
+				```"
+		end
+
+		entry do
+			name 'Close Window'
+			notes "
+				```
+				⌘ + ⇧ + W
+				```"
+		end
+
+		entry do
+			name 'Show Sidebar'
+			notes "
+				```
+				⌘ + K, ⌘ + B
+				```"
+		end
+
+		entry do
+			name 'Show Console'
+			notes "
+				```
+				⌃ + `
+				```"
+		end
+
+		entry do
+			name 'Enter Full Screen'
+			notes "
+				```
+				⌘ + ⌃ + F
+				```"
+		end
+
+		entry do
+			name 'Enter Distra­ction Free Mode'
+			notes "
+				```
+				⌘ + ⌃ + ⇧ + F
+				```"
+		end
+	end
+
+	category do
+		id 'Moving / Editing'
+
+		entry do
+			name 'Undo'
+			notes "
+				```
+				⌘ + Z
+				```"
+		end
+
+		entry do
+			name 'Redo'
+			notes "
+				```
+				⌘ + ⇧ + Z
+				```"
+		end
+
+		entry do
+			name 'Repeat'
+			notes "
+				```
+				⌘ + Y
+				```"
+		end
+
+		entry do
+			name 'Copy'
+			notes "
+				```
+				⌘ + C
+				```"
+		end
+
+		entry do
+			name 'Yank (inserts the text that’s on top of the kill ring)'
+			notes "
+				```
+				⌘ + Y
+				```"
+		end
+
+		entry do
+			name 'Cut'
+			notes "
+				```
+				⌘ + X
+				```"
+		end
+
+		entry do
+			name 'Paste'
+			notes "
+				```
+				⌘ + V
+				```"
+		end
+
+		entry do
+			name 'Paste and Indent'
+			notes "
+				```
+				⌘ + ⇧ + V
+				```"
+		end
+
+		entry do
+			name 'Indent'
+			notes "
+				```
+				⌘ + ]
+				```"
+		end
+
+		entry do
+			name 'Unindent'
+			notes "
+				```
+				⌘ + [
+				```"
+		end
+
+		entry do
+			name 'Swap Line Up/Down'
+			notes "
+				```
+				⌘ + ⌃ + ↑/↓
+				```"
+		end
+
+		entry do
+			name 'Duplicate Line'
+			notes "
+				```
+				⌘ + ⇧ + D
+				```"
+		end
+
+		entry do
+			name 'Delete (kill) Line'
+			notes "
+				```
+				⌃ + ⇧ + K
+				```"
+		end
+
+		entry do
+			name 'Join Lines'
+			notes "
+				```
+				⌘ + J
+				```"
+		end
+
+		entry do
+			name 'Toggle Comment'
+			notes "
+				```
+				⌘ + /
+				```"
+		end
+
+		entry do
+			name 'Toggle Block Comment'
+			notes "
+				```
+				⌘ + ⌥ + /
+				```"
+		end
+
+		entry do
+			name 'Insert Line Before'
+			notes "
+				```
+				⌘ + ⇧ + ↩
+				```"
+		end
+
+		entry do
+			name 'Insert Line After'
+			notes "
+				```
+				⌘ + ↩
+				```"
+		end
+
+		entry do
+			name 'Delete to Beginning of Line'
+			notes "
+				```
+				⌘ + ⌫
+				```"
+		end
+
+		entry do
+			name 'Delete to End of Line'
+			notes "
+				```
+				⌃ + K
+				```"
+		end
+
+		entry do
+			name 'Move to Beginning of Line'
+			notes "
+				```
+				⌃ + A
+				```"
+		end
+
+		entry do
+			name 'Move to End of Line'
+			notes "
+				```
+				⌃ + E
+				```"
+		end
+
+		entry do
+			name '(Previous) Move Up'
+			notes "
+				```
+				⌃ + P
+				```"
+		end
+
+		entry do
+			name '(Forward) Move Right'
+			notes "
+				```
+				⌃ + F
+				```"
+		end
+
+		entry do
+			name '(Next) Move Down'
+			notes "
+				```
+				⌃ + N
+				```"
+		end
+
+		entry do
+			name '(Back) Move Left'
+			notes "
+				```
+				⌃ + B
+				```"
+		end
+
+		entry do
+			name 'Transpose (swap two characters or words; buggy behaviour)'
+			notes "
+				```
+				⌃ + T
+				```"
+		end
+
+		entry do
+			name 'Close Tag'
+			notes "
+				```
+				⌘ + ⌥ + .
+				```"
+		end
+
+		entry do
+			name 'Wrap Selection with Tag'
+			notes "
+				```
+				⌃ + ⇧ + W
+				```"
+		end
+
+		entry do
+			name 'Convert Selected Text to Uppercase'
+			notes "
+				```
+				⌘ + K, ⌘ + U
+				```"
+		end
+
+		entry do
+			name 'Convert Selected Text to Lowercase'
+			notes "
+				```
+				⌘ + K, ⌘ + L
+				```"
+		end
+
+		entry do
+			name 'Wrap Paragraph at Ruler'
+			notes "
+				```
+				⌘ + ⌥ + Q
+				```"
+		end
+
+		entry do
+			name 'Sort Lines'
+			notes "
+				```
+				F5
+				```"
+		end
+
+		entry do
+			name 'Sort Lines (Case Sensitive)'
+			notes "
+				```
+				⌃ + F5
+				```"
+		end
+
+		entry do
+			name 'Goto Line'
+			notes "
+				```
+				⌃ + G
+				```"
+		end
+
+		entry do
+			name 'Jump to Matching Brackets (does not work with HTML/XML tags)'
+			notes "
+				```
+				⌃ + M
+				```"
+		end
+
+		entry do
+			name 'Scroll to Selection (centers the screen to cursor)'
+			notes "
+				```
+				⌃ + L
+				```"
+		end
+
+		entry do
+			name 'Scroll Line Up/Down'
+			notes "
+				```
+				⌃ + ⌥ + ↑/↓
+				```"
+		end
+
+		entry do
+				name 'Record Macro'
+				notes "
+					```
+					⌃ + Q
+					```"
+		end
+
+		entry do
+			name 'Playback Macro'
+			notes "
+				```
+				⌃ + ⇧ + Q
+				```"
+		end
+	end
+
+	category do
+		id 'Selection'
+
+		entry do
+			name 'Undo'
+			notes "
+				```
+				⌘ + U
+				```"
+		end
+
+		entry do
+			name 'Soft Undo'
+			notes "
+				```
+				⌘ + ⇧ + U
+				```"
+		end
+
+		entry do
+			name 'Split Selection Into Lines (inserts multiple cursors)'
+			notes "
+				```
+				⌘ + ⇧ + L
+				```"
+		end
+
+		entry do
+			name 'Add Previo­us/Next Line (inserts multiple cursors)'
+			notes "
+				```
+				⌃ + ⇧ + ↑/↓
+				```"
+		end
+
+		entry do
+			name 'Single Selection (when there are multiple select­ions; exits to visual mode first if you are in insert mode)'
+			notes "
+				```
+				⎋
+				```"
+		end
+
+		entry do
+			name 'Select All'
+			notes "
+				```
+				⌘ + A
+				```"
+		end
+
+		entry do
+			name 'Expand Selection to Word'
+			notes "
+				```
+				⌘ + D
+				```"
+		end
+
+		entry do
+			name 'Expand Selection to Line'
+			notes "
+				```
+				⌘ + L
+				```"
+		end
+
+		entry do
+			name 'Expand Selection to Tag (HTML/XML)'
+			notes "
+				```
+				⌘ + ⇧ + A
+				```"
+		end
+
+		entry do
+			name 'Expand Selection to Scope'
+			notes "
+				```
+				⌘ + ⇧ + Space
+				```"
+		end
+
+		entry do
+			name 'Expand Selection to Brackets'
+			notes "
+				```
+				⌃ + ⇧ + M
+				```"
+		end
+
+		entry do
+			name 'Expand Selection to Indent­ation'
+			notes "
+				```
+				⌘ + ⇧ + J
+				```"
+		end
+	end
+
+	category do
+		id 'Search / Replace'
+
+		entry do
+			name 'Find'
+			notes "
+				```
+				⌘ +
+				```"
+		end
+
+		entry do
+			name 'Find Next'
+			notes "
+				```
+				⌘ + G
+				```"
+		end
+
+		entry do
+			name 'Find Previous'
+			notes "
+				```
+				⌘ + ⇧ + G
+				```"
+		end
+
+		entry do
+			name 'Increm­ental Find'
+			notes "
+				```
+				⌘ + I
+				```"
+		end
+
+		entry do
+			name 'Replace'
+			notes "
+				```
+				⌘ + ⌥ + F
+				```"
+		end
+
+		entry do
+			name 'Replace Next'
+			notes "
+				```
+				⌘ + ⌥ + E
+				```"
+		end
+
+		entry do
+			name 'Quick Find (searches for the word under the cursor)'
+			notes "
+				```
+				⌘ + ⌥ + G
+				```"
+		end
+
+		entry do
+			name 'Quick Find All (selects all occurences of the word under the cursor)'
+			notes "
+				```
+				⌘ + ⌃ + G
+				```"
+		end
+
+		entry do
+			name 'Use Selection for Find'
+			notes "
+				```
+				⌘ + E
+				```"
+		end
+
+		entry do
+			name 'Use Selection for Replace'
+			notes "
+				```
+				⌘ + ⇧ + E
+				```"
+		end
+
+		entry do
+			name 'Find in Files (all open files)'
+			notes "
+				```
+				⌘ + ⇧ + F
+				```"
+		end
+
+		entry do
+			name 'Next Result (file search results)'
+			notes "
+				```
+				F4
+				```"
+		end
+
+		entry do
+			name 'Previous Result (file search results)'
+			notes "
+				```
+				⇧ + F4
+				```"
+		end
+	end
+
+	category do
+		id 'Folding'
+
+		entry do
+			name 'Fold (selec­tion)'
+			notes "
+				```
+				⌘ + ⌥ + [
+				```"
+		end
+
+		entry do
+			name 'Unfold'
+			notes "
+				```
+				⌘ + ⌥ + ]
+				```"
+		end
+
+		entry do
+			name 'Unfold All'
+			notes "
+				```
+				⌘ + K, ⌘ + J
+				```"
+		end
+
+		entry do
+			name 'Fold All'
+			notes "
+				```
+				⌘ + K, ⌘ + 1
+				```"
+		end
+
+		entry do
+			name 'Fold Level 2-9'
+			notes "
+				```
+				⌘ + K, ⌘ + 2-9
+				```"
+		end
+
+		entry do
+			name 'Fold Tag Attributes (HTML/XML)'
+			notes "
+				```
+				⌘ + K, ⌘ + T
+				```"
+		end
+	end
+
+	category do
+		id 'Bookmarking'
+
+		entry do
+			name 'Set Mark'
+			notes "
+				```
+				⌘ + K, ⌘ + Space
+				```"
+		end
+
+		entry do
+			name 'Select to Mark'
+			notes "
+				```
+				⌘ + K, ⌘ + A
+				```"
+		end
+
+		entry do
+			name 'Delete to Mark'
+			notes "
+				```
+				⌘ + K, ⌘ + W
+				```"
+		end
+
+		entry do
+			name 'Swap with Mark'
+			notes "
+				```
+				⌘ + K, ⌘ + X
+				```"
+		end
+
+		entry do
+			name 'Clear to Mark'
+			notes "
+				```
+				⌘ + K, ⌘ + G
+				```"
+		end
+
+		entry do
+			name 'Toggle Bookmark'
+			notes "
+				```
+				⌘ + F2
+				```"
+		end
+
+		entry do
+			name 'Nex Bookmark'
+			notes "
+				```
+				F2
+				```"
+		end
+
+		entry do
+			name 'Previous Bookmark'
+			notes "
+				```
+				⇧ + F2
+				```"
+		end
+
+		entry do
+			name 'Clear Bookmarks'
+			notes "
+				```
+				⌘ + ⇧ + F2
+				```"
+		end
+	end
+
+	category do
+		id 'Miscellaneous'
+
+		entry do
+			name 'Toggle Spell Check'
+			notes "
+				```
+				F6
+				```"
+		end
+
+		entry do
+			name 'Next Misspe­lling'
+			notes "
+				```
+				⌃ + F6
+				```"
+		end
+
+		entry do
+			name 'Previous Misspe­lling'
+			notes "
+				```
+				⌃ + ⇧ + F6
+				```"
+		end
+
+		entry do
+			name 'Build'
+			notes "
+				```
+				⌘ + B
+				```"
+		end
+
+		entry do
+			name 'Show Comple­tions'
+			notes "
+				```
+				⌃ + Space
+				```"
+		end
+
+		entry do
+			name 'Special Characters'
+			notes "
+				```
+				⌘ + ⌥ + T
+				```"
+		end
+	end
+
+
+  notes <<-'END'
+    * Based on the cheat sheet from [Cheatography](http://www.cheatography.com/martinprins/cheat-sheets/sublime-text-3-osx/).
+    * Converted and extended by [Rob Simpson](https://github.com/pertrai1).
+  END
+end
