f83ce6d7470db2ae29ed27018b2bd1cf1ca3d755
Dan Gilbert <dan.gilbert@smilemachine.com>
2014-03-19 22:46:05 +0000

    Added Java 8 Lambda Syntax cheatsheet

---
+++ cheatsheets/Java_Lambda_Syntax.rb
@@ -0,0 +1,57 @@
+cheatsheet do
+  title 'Java Lambda Syntax'
+  docset_file_name 'Java_Lambda_Syntax'
+  keyword 'java'
+  source_url 'http://cheat.kapeli.com'
+
+  category do
+    id 'Java Lambda Syntax'
+    entry do
+        name 'As an expression'
+        notes <<-'END'
+        ```java
+        (Person p) -> p.getGender() == Person.Sex.MALE
+            && p.getAge() >= 18
+            && p.getAge() <= 25
+        ```
+        END
+    end
+    entry do
+        name 'As a local variable'
+        notes <<-'END'
+        ```java
+        @FunctionalInterface
+        public interface SomeFunction {
+            public void doWork();
+        }
+
+        SomeFunction lambdaName = () -> {...};
+        ```
+        END
+    end
+    entry do
+        name 'As a method parameter'
+        notes <<-'END'
+        ```java
+        @FunctionalInterface
+        public interface SomeFunction {
+            public void doWork();
+        }
+
+        void someMethodThatTakesALambda(SomeFunction lambdaName) {...}
+        ```
+        END
+    end
+    entry do
+        name 'As an argument to a method call'
+        notes <<-'END'
+        ```java
+        someObject.someMethodThatTakesALambda ((ParameterClass p) -> {...});
+        ```
+        END
+    end
+  end
+  notes <<-'END'
+    * Based on a [cheat sheet](http://fuckingjavalambdasyntax.com/) by [Alex Florescu](http://florescu.org/)
+  END
+end
