b5b484a086e2bc288f13088bffbf2c257b674e1e
Yu-Cheng Chuang <ducksteven@gmail.com>
2014-03-29 21:59:15 +0800

    remove printf debug

--- cheatsheets/Font_Awesome.rb
+++ cheatsheets/Font_Awesome.rb
@@ -52,7 +52,6 @@ end
 
 # path to resources
 RES_DIR = File.expand_path("../resources/Font_Awesome", __FILE__)
-puts RES_DIR
 
 # checkout dir of Font-Awesome official repository
 FA_SRC_DIR = File.join(RES_DIR, "Font-Awesome")
