f0a89bcf0d853769f7ba19b16fd727bba6cecb05
Bogdan Popescu <bogdansrc@gmail.com>
2014-04-06 04:06:59 +0300

    iOS Design Cheat Sheet

---
+++ cheatsheets/iOS_Design.rb
@@ -0,0 +1,341 @@
+cheatsheet do
+  title 'iOS Design'
+  docset_file_name 'iOS_Design'
+  keyword 'iosd'
+  source_url 'http://cheat.kapeli.com'
+
+  style '
+  
+  td.td_notes code {
+    border: none;
+    box-shadow: none;
+    background: none;
+    font-family: "Open Sans", sans-serif;
+    font-size: medium;
+    color: #666666;
+  }
+
+  td, th {
+    white-space:nowrap;
+    min-width:110px;
+  }
+
+  .no-min-width td, .no-min-width th {
+    min-width:0;
+  }
+
+  td:last-child {
+    width:99%;
+  }
+ '
+
+# ~~~~~~~~~~~~ Table - Resolutions ~~~~~~~~~~~~
+category do
+  id 'Resolutions'
+  header 'Device'
+  header 'Portrait'
+  header 'Landscape'
+
+  entry do
+    td_notes '640 x 1136 px'
+    td_notes '1136 x 640 px'
+    name 'iPhone 5'
+    index_name 'iPhone 5 Resolution'
+    notes 'iPhone 5, 5S, 5C'
+  end
+
+  entry do
+    td_notes '640 x 960 px'
+    td_notes '960 x 640 px'
+    name 'iPhone 4/4S'
+    index_name 'iPhone 4/4S Resolution'
+    notes ''
+  end
+
+  entry do
+    td_notes '320 x 480 px'
+    td_notes '480 x 320 px'
+    name 'iPhone & iPod Touch'
+    index_name 'iPhone & iPod Touch Resolution'
+    notes '1st, 2nd and 3rd Generation'
+  end
+
+  entry do
+    td_notes '1536 x 2048 px'
+    td_notes '2048 x 1536 px'
+    name 'iPad Retina'
+    index_name 'iPad Retina Resolution'
+    notes 'iPad 3, iPad 4, iPad Air'
+  end
+
+  entry do
+    td_notes '768 x 1024 px'
+    td_notes '1024 x 768 px'
+    name 'iPad Mini'
+    index_name 'iPad Mini Resolution'
+    notes ''
+  end
+
+  entry do
+    td_notes '1536 x 2048 px'
+    td_notes '2048 x 1536 px'
+    name 'iPad Mini Retina'
+    index_name 'iPad Mini Retina Resolution'
+    notes ''
+  end
+
+  entry do
+    td_notes '768 x 1024 px'
+    td_notes '1024 x 768 px'
+    name 'iPad'
+    index_name 'iPad Resolution'
+    notes '1st and 2nd Generation'
+  end
+
+end
+
+# ~~~~~~~~~~~~ Table - Displays ~~~~~~~~~~~~
+category do
+  id 'Displays'
+  header 'Device'
+  header 'PPI'
+  header 'Color Mode'
+  header 'Color Temperature'
+
+  entry do
+    td_notes '326'
+    td_notes '8bit RGB'
+    td_notes  'Warm'
+    name 'iPhone 5'
+    index_name 'iPhone 5 Display'
+    notes 'iPhone 5, 5S, 5C'
+  end
+
+  entry do
+    td_notes '326'
+    td_notes '8bit RGB'
+    td_notes  'Cool'
+    name 'iPhone 4/4S'
+    index_name 'iPhone 4/4S Display'
+    notes ''
+  end
+
+  entry do
+    td_notes '163'
+    td_notes '8bit RGB'
+    td_notes  'Warm'
+    name 'iPhone & iPod Touch'
+    index_name 'iPhone & iPod Touch Display'
+    notes '1st, 2nd and 3rd Generation'
+  end
+
+  entry do
+    td_notes '264'
+    td_notes '8bit RGB'
+    td_notes  'Warm'
+    name 'iPad Retina'
+    index_name 'iPad Retina Display'
+    notes 'iPad 3, iPad 4, iPad Air'
+  end
+
+  entry do
+    td_notes '163'
+    td_notes '8bit RGB'
+    td_notes  'Warm'
+    name 'iPad Mini'
+    index_name 'iPad Mini Display'
+    notes ''
+  end
+
+  entry do
+    td_notes '326'
+    td_notes '8bit RGB'
+    td_notes  'Warm'
+    name 'iPad Mini Retina'
+    index_name 'iPad Mini Retina Display'
+    notes ''
+  end
+
+  entry do
+    td_notes '132'
+    td_notes '8bit RGB'
+    td_notes  'Warm'
+    name 'iPad'
+    index_name 'iPad Display'
+    notes '1st and 2nd Generation'
+  end
+
+end
+
+# ~~~~~~~~~~~~ Table - App Icons ~~~~~~~~~~~~
+category do
+  id 'Dimensions'
+  header 'Device'
+  header 'App Icon'
+  header 'App Store Icon'
+  header 'Spotlight Icon'
+  header 'Settings Icon'
+
+  entry do
+    td_notes '120 x 120 px'
+    td_notes '1024 x 1024 px'
+    td_notes '80 x 80 px'
+    td_notes '58 x 58 px'
+    name 'iPhone 5'
+    index_name 'iPhone 5 Dimensions'
+    notes 'iPhone 5, 5S, 5C'
+  end
+
+  entry do
+    td_notes '120 x 120 px'
+    td_notes '1024 x 1024 px'
+    td_notes '80 x 80 px'
+    td_notes '58 x 58 px'
+    name 'iPhone 4/4S'
+    index_name 'iPhone 4/4S Dimensions'
+    notes ''
+  end
+
+  entry do
+    td_notes '152 x 152 px'
+    td_notes '1024 x 1024 px'
+    td_notes '80 x 80 px'
+    td_notes '58 x 58 px'
+    name 'iPad Retina'
+    index_name 'iPad Retina Dimensions'
+    notes 'iPad 3, 4, Air, iPad Mini Retina'
+  end
+
+  entry do
+    td_notes '76 x 76 px'
+    td_notes '512 x 512 px'
+    td_notes '40 x 40 px'
+    td_notes '29 x 29 px'
+    name 'iPad Mini'
+    index_name 'iPad Mini Dimensions'
+    notes ''
+  end
+
+  entry do
+   td_notes '76 x 76 px'
+   td_notes '512 x 512 px'
+   td_notes '40 x 40 px'
+   td_notes '29 x 29 px'
+   name 'iPad'
+    index_name 'iPad Dimensions'
+   notes '1st and 2nd Generation'
+ end
+
+end
+
+# ~~~~~~~~~~~~ Table - Common Icons ~~~~~~~~~~~~
+category do
+  id 'Common Design Elements'
+  header 'Device'
+  header 'Status Bar Height'
+  header 'Navigation Bar Height'
+  header 'Tab Bar Height'
+  header 'Table Width'
+  html_class 'no-min-width'
+
+  entry do
+    td_notes '40 px'
+    td_notes '88 / 64 px'
+    td_notes '98 px'
+    td_notes '640 / 1136 px'
+    name 'iPhone 5'
+    index_name 'iPhone 5 Design Elements'
+    notes 'iPhone 5, 5S, 5C'
+  end
+
+  entry do
+    td_notes '40 px'
+    td_notes '88 / 64 px'
+    td_notes '98 px'
+    td_notes '640 / 960 px'
+    name 'iPhone 4/4S'
+    index_name 'iPhone 4/4S Design Elements'
+    notes ''
+  end
+
+
+  entry do
+    td_notes '40 px'
+    td_notes '88 px'
+    td_notes '112 px'
+    td_notes 'dynamic'
+    name 'iPad Retina'
+    index_name 'iPad Retina Design Elements'
+    notes 'iPad 3, 4, Air, iPad Mini Retina'
+  end
+
+  entry do
+    td_notes '20 px'
+    td_notes '44 px'
+    td_notes '56 px'
+    td_notes 'dynamic'
+    name 'iPad Mini'
+    index_name 'iPad Mini Design Elements'
+    notes ''
+  end
+
+  entry do
+    td_notes '20 px'
+    td_notes '44 px'
+    td_notes '56 px'
+    td_notes 'dynamic'
+    name 'iPad'
+    index_name 'iPad Design Elements'
+    notes '1st and 2nd Generation'
+  end
+
+end
+
+
+# ~~~~~~~~~~~~ Table - Default Font Sizes ~~~~~~~~~~~~
+category do
+  id 'Default Font Sizes'
+  header 'Label Type'
+  header 'Default Font Size'
+  header 'Default Font Weight'
+
+  entry do
+    td_notes '34 px'
+    td_notes 'Medium'
+    name 'Navigation bar title'
+  end
+
+  entry do
+    td_notes '34 px'
+    td_notes 'Light'
+    name 'Regular buttons'
+  end
+
+  entry do
+    td_notes '34 px'
+    td_notes 'Light'
+    name 'Table header'
+  end
+
+  entry do
+    td_notes '28 px'
+    td_notes 'Regular'
+    name 'Table label'
+  end
+
+  entry do
+    td_notes '20 px'
+    td_notes 'Regular'
+    name 'Tab bar icon labels'
+  end
+
+end
+
+
+# ~~~~~~~~~~~~ Footer ~~~~~~~~~~~~
+notes <<-'END'
+* Based on [iOS7 Cheat Sheet](http://ivomynttinen.com/blog/the-ios-7-design-cheat-sheet) by [Ivo Mynttinen](http://ivomynttinen.com)
+* Converted by [Deepu Mukundan](http://about.me/deepumukundan)
+END
+
+end
