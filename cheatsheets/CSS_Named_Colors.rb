ecb519375607ac85b57e5ef2deb6e10d838aac97
Tyler Eich <tyler2eich@gmail.com>
2014-04-13 10:51:47 -0400

    CSS Named Colors

---
+++ cheatsheets/CSS_Named_Colors.rb
@@ -0,0 +1,714 @@
+cheatsheet do
+  title 'CSS Named Colors'
+  docset_file_name 'CSS Named Colors'
+  keyword 'color'
+  source_url 'cheat.kapeli.com'
+  
+  category do
+    id 'Standard Colors'
+    
+    entry do
+      name 'Aqua'
+      notes '#00FFFF'
+    end
+    
+    entry do
+      name 'Black'
+      notes '#000000'
+    end
+    
+    entry do
+      name 'Blue'
+      notes '#0000FF'
+    end
+    
+    entry do
+      name 'Fuchsia'
+      notes '#FF00FF'
+    end
+    
+    entry do
+      name 'Gray'
+      notes '#808080'
+    end
+    
+    entry do
+      name 'Green'
+      notes '#008000'
+    end
+    
+    entry do
+      name 'Lime'
+      notes '#00FF00'
+    end
+    
+    entry do
+      name 'Maroon'
+      notes '#800000'
+    end
+    
+    entry do
+      name 'Navy'
+      notes '#000080'
+    end
+    
+    entry do
+      name 'Olive'
+      notes '#808000'
+    end
+    
+    entry do
+      name 'Orange'
+      notes '#FFA500'
+    end
+    
+    entry do
+      name 'Purple'
+      notes '#800080'
+    end
+    
+    entry do
+      name 'Red'
+      notes '#FF0000'
+    end
+    
+    entry do
+      name 'Silver'
+      notes '#C0C0C0'
+    end
+    
+    entry do
+      name 'Teal'
+      notes '#008080'
+    end
+    
+    entry do
+      name 'White'
+      notes '#FFFFFF'
+    end
+    
+    entry do
+      name 'Yellow'
+      notes '#FFFF00'
+    end
+  end
+  
+  category do
+    id 'Extended Colors'
+    
+    entry do
+      name 'AliceBlue'
+      notes '#F0F8FF'
+    end
+    
+    entry do
+      name 'AntiqueWhite'
+      notes '#FAEBD7'
+    end
+    
+    entry do
+      name 'Aquamarine'
+      notes '#7FFFD4'
+    end
+    
+    entry do
+      name 'Azure'
+      notes '#F0FFFF'
+    end
+    
+    entry do
+      name 'Beige'
+      notes '#F5F5DC'
+    end
+    
+    entry do
+      name 'Bisque'
+      notes '#FFE4C4'
+    end
+    
+    entry do
+      name 'BlanchedAlmond'
+      notes '#FFEBCD'
+    end
+    
+    entry do
+      name 'BlueViolet'
+      notes '#8A2BE2'
+    end
+    
+    entry do
+      name 'Brown'
+      notes '#A52A2A'
+    end
+    
+    entry do
+      name 'BurlyWood'
+      notes '#DEB887'
+    end
+    
+    entry do
+      name 'CadetBlue'
+      notes '#5F9EA0'
+    end
+    
+    entry do
+      name 'Chartreuse'
+      notes '#7FFF00'
+    end
+    
+    entry do
+      name 'Chocolate'
+      notes '#D2691E'
+    end
+    
+    entry do
+      name 'Coral'
+      notes '#FF7F50'
+    end
+    
+    entry do
+      name 'CornflowerBlue'
+      notes '#6495ED'
+    end
+    
+    entry do
+      name 'Cornsilk'
+      notes '#FFF8DC'
+    end
+    
+    entry do
+      name 'Crimson'
+      notes '#DC143C'
+    end
+    
+    entry do
+      name 'Cyan'
+      notes '#00FFFF'
+    end
+    
+    entry do
+      name 'DarkBlue'
+      notes '#00008B'
+    end
+    
+    entry do
+      name 'DarkCyan'
+      notes '#008B8B'
+    end
+    
+    entry do
+      name 'DarkGoldenRod'
+      notes '#B8860B'
+    end
+    
+    entry do
+      name 'DarkGray'
+      notes '#A9A9A9'
+    end
+    
+    entry do
+      name 'DarkGreen'
+      notes '#006400'
+    end
+    
+    entry do
+      name 'DarkKhaki'
+      notes '#BDB76B'
+    end
+    
+    entry do
+      name 'DarkMagenta'
+      notes '#8B008B'
+    end
+    
+    entry do
+      name 'DarkOliveGreen'
+      notes '#556B2F'
+    end
+    
+    entry do
+      name 'DarkOrange'
+      notes '#FF8C00'
+    end
+    
+    entry do
+      name 'DarkOrchid'
+      notes '#9932CC'
+    end
+    
+    entry do
+      name 'DarkRed'
+      notes '#8B0000'
+    end
+    
+    entry do
+      name 'DarkSalmon'
+      notes '#E9967A'
+    end
+    
+    entry do
+      name 'DarkSeaGreen'
+      notes '#8FBC8F'
+    end
+    
+    entry do
+      name 'DarkSlateBlue'
+      notes '#483D8B'
+    end
+    
+    entry do
+      name 'DarkSlateGray'
+      notes '#2F4F4F'
+    end
+    
+    entry do
+      name 'DarkTurquoise'
+      notes '#00CED1'
+    end
+    
+    entry do
+      name 'DarkViolet'
+      notes '#9400D3'
+    end
+    
+    entry do
+      name 'DeepPink'
+      notes '#FF1493'
+    end
+    
+    entry do
+      name 'DeepSkyBlue'
+      notes '#00BFFF'
+    end
+    
+    entry do
+      name 'DimGray'
+      notes '#696969'
+    end
+    
+    entry do
+      name 'DodgerBlue'
+      notes '#1E90FF'
+    end
+    
+    entry do
+      name 'FireBrick'
+      notes '#B22222'
+    end
+    
+    entry do
+      name 'FloralWhite'
+      notes '#FFFAF0'
+    end
+    
+    entry do
+      name 'ForestGreen'
+      notes '#228B22'
+    end
+    
+    entry do
+      name 'Gainsboro'
+      notes '#DCDCDC'
+    end
+    
+    entry do
+      name 'GhostWhite'
+      notes '#F8F8FF'
+    end
+    
+    entry do
+      name 'Gold'
+      notes '#FFD700'
+    end
+    
+    entry do
+      name 'GoldenRod'
+      notes '#DAA520'
+    end
+    
+    entry do
+      name 'GreenYellow'
+      notes '#ADFF2F'
+    end
+    
+    entry do
+      name 'HoneyDew'
+      notes '#F0FFF0'
+    end
+    
+    entry do
+      name 'HotPink'
+      notes '#FF69B4'
+    end
+    
+    entry do
+      name 'IndianRed '
+      notes '#CD5C5C'
+    end
+    
+    entry do
+      name 'Indigo '
+      notes '#4B0082'
+    end
+    
+    entry do
+      name 'Ivory'
+      notes '#FFFFF0'
+    end
+    
+    entry do
+      name 'Khaki'
+      notes '#F0E68C'
+    end
+    
+    entry do
+      name 'Lavender'
+      notes '#E6E6FA'
+    end
+    
+    entry do
+      name 'LavenderBlush'
+      notes '#FFF0F5'
+    end
+    
+    entry do
+      name 'LawnGreen'
+      notes '#7CFC00'
+    end
+    
+    entry do
+      name 'LemonChiffon'
+      notes '#FFFACD'
+    end
+    
+    entry do
+      name 'LightBlue'
+      notes '#ADD8E6'
+    end
+    
+    entry do
+      name 'LightCoral'
+      notes '#F08080'
+    end
+    
+    entry do
+      name 'LightCyan'
+      notes '#E0FFFF'
+    end
+    
+    entry do
+      name 'LightGoldenRodYellow'
+      notes '#FAFAD2'
+    end
+    
+    entry do
+      name 'LightGray'
+      notes '#D3D3D3'
+    end
+    
+    entry do
+      name 'LightGreen'
+      notes '#90EE90'
+    end
+    
+    entry do
+      name 'LightPink'
+      notes '#FFB6C1'
+    end
+    
+    entry do
+      name 'LightSalmon'
+      notes '#FFA07A'
+    end
+    
+    entry do
+      name 'LightSeaGreen'
+      notes '#20B2AA'
+    end
+    
+    entry do
+      name 'LightSkyBlue'
+      notes '#87CEFA'
+    end
+    
+    entry do
+      name 'LightSlateGray'
+      notes '#778899'
+    end
+    
+    entry do
+      name 'LightSteelBlue'
+      notes '#B0C4DE'
+    end
+    
+    entry do
+      name 'LightYellow'
+      notes '#FFFFE0'
+    end
+    
+    entry do
+      name 'LimeGreen'
+      notes '#32CD32'
+    end
+    
+    entry do
+      name 'Linen'
+      notes '#FAF0E6'
+    end
+    
+    entry do
+      name 'Magenta'
+      notes '#FF00FF'
+    end
+    
+    entry do
+      name 'MediumAquaMarine'
+      notes '#66CDAA'
+    end
+    
+    entry do
+      name 'MediumBlue'
+      notes '#0000CD'
+    end
+    
+    entry do
+      name 'MediumOrchid'
+      notes '#BA55D3'
+    end
+    
+    entry do
+      name 'MediumPurple'
+      notes '#9370DB'
+    end
+    
+    entry do
+      name 'MediumSeaGreen'
+      notes '#3CB371'
+    end
+    
+    entry do
+      name 'MediumSlateBlue'
+      notes '#7B68EE'
+    end
+    
+    entry do
+      name 'MediumSpringGreen'
+      notes '#00FA9A'
+    end
+    
+    entry do
+      name 'MediumTurquoise'
+      notes '#48D1CC'
+    end
+    
+    entry do
+      name 'MediumVioletRed'
+      notes '#C71585'
+    end
+    
+    entry do
+      name 'MidnightBlue'
+      notes '#191970'
+    end
+    
+    entry do
+      name 'MintCream'
+      notes '#F5FFFA'
+    end
+    
+    entry do
+      name 'MistyRose'
+      notes '#FFE4E1'
+    end
+    
+    entry do
+      name 'Moccasin'
+      notes '#FFE4B5'
+    end
+    
+    entry do
+      name 'NavajoWhite'
+      notes '#FFDEAD'
+    end
+    
+    entry do
+      name 'OldLace'
+      notes '#FDF5E6'
+    end
+    
+    entry do
+      name 'OliveDrab'
+      notes '#6B8E23'
+    end
+    
+    entry do
+      name 'OrangeRed'
+      notes '#FF4500'
+    end
+    
+    entry do
+      name 'Orchid'
+      notes '#DA70D6'
+    end
+    
+    entry do
+      name 'PaleGoldenRod'
+      notes '#EEE8AA'
+    end
+    
+    entry do
+      name 'PaleGreen'
+      notes '#98FB98'
+    end
+    
+    entry do
+      name 'PaleTurquoise'
+      notes '#AFEEEE'
+    end
+    
+    entry do
+      name 'PaleVioletRed'
+      notes '#DB7093'
+    end
+    
+    entry do
+      name 'PapayaWhip'
+      notes '#FFEFD5'
+    end
+    
+    entry do
+      name 'PeachPuff'
+      notes '#FFDAB9'
+    end
+    
+    entry do
+      name 'Peru'
+      notes '#CD853F'
+    end
+    
+    entry do
+      name 'Pink'
+      notes '#FFC0CB'
+    end
+    
+    entry do
+      name 'Plum'
+      notes '#DDA0DD'
+    end
+    
+    entry do
+      name 'PowderBlue'
+      notes '#B0E0E6'
+    end
+    
+    entry do
+      name 'RosyBrown'
+      notes '#BC8F8F'
+    end
+    
+    entry do
+      name 'RoyalBlue'
+      notes '#4169E1'
+    end
+    
+    entry do
+      name 'SaddleBrown'
+      notes '#8B4513'
+    end
+    
+    entry do
+      name 'Salmon'
+      notes '#FA8072'
+    end
+    
+    entry do
+      name 'SandyBrown'
+      notes '#F4A460'
+    end
+    
+    entry do
+      name 'SeaGreen'
+      notes '#2E8B57'
+    end
+    
+    entry do
+      name 'SeaShell'
+      notes '#FFF5EE'
+    end
+    
+    entry do
+      name 'Sienna'
+      notes '#A0522D'
+    end
+    
+    entry do
+      name 'SkyBlue'
+      notes '#87CEEB'
+    end
+    
+    entry do
+      name 'SlateBlue'
+      notes '#6A5ACD'
+    end
+    
+    entry do
+      name 'SlateGray'
+      notes '#708090'
+    end
+    
+    entry do
+      name 'Snow'
+      notes '#FFFAFA'
+    end
+    
+    entry do
+      name 'SpringGreen'
+      notes '#00FF7F'
+    end
+    
+    entry do
+      name 'SteelBlue'
+      notes '#4682B4'
+    end
+    
+    entry do
+      name 'Tan'
+      notes '#D2B48C'
+    end
+    
+    entry do
+      name 'Thistle'
+      notes '#D8BFD8'
+    end
+    
+    entry do
+      name 'Tomato'
+      notes '#FF6347'
+    end
+    
+    entry do
+      name 'Turquoise'
+      notes '#40E0D0'
+    end
+    
+    entry do
+      name 'Violet'
+      notes '#EE82EE'
+    end
+    
+    entry do
+      name 'Wheat'
+      notes '#F5DEB3'
+    end
+    
+    entry do
+      name 'WhiteSmoke'
+      notes '#F5F5F5'
+    end
+    
+    entry do
+      name 'YellowGreen'
+      notes '#9ACD32'
+    end
+  end
+end
