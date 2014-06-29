32de487b5c20c92eff997a73ddc94612ab2f7e68
Jacob Reiff <jacob@skyscraper.io>
2014-06-29 10:44:02 -0700

    Added cheatsheet for Origami, a Sublime Text package.

---
+++ cheatsheets/Origami.rb
@@ -0,0 +1,55 @@
+cheatsheet do
+    title 'Origami for Sublime Text'
+    docset_file_name 'origami'
+    keyword 'origami'
+    source_url 'http://cheat.kapeli.com'
+
+    category do
+        id 'Panes'
+
+        entry do
+            name 'Travel focus to adjacent pane'
+            command 'CMD+K Arrow'
+        end
+
+        entry do
+            name 'Carry current file to adjacent pane'
+            command 'CMD+K SHIFT-Arrow '
+        end
+
+        entry do
+            name 'Clone current file to adjacent pane'
+            command 'CMD+K OPT-Arrow'
+        end
+
+        entry do
+            name 'Create an adjacent pane'
+            command 'CMD+K CMD-Arrow'
+        end
+
+        entry do
+            name 'Destroy an adjacent pane'
+            command 'CMD+K CMD-SHIFT-Arrow'
+        end
+    end
+
+    category do
+        id 'Zoom / Unzoom'
+
+        entry do
+            name 'Zoom on current pane'
+            command 'CMD+K CMD+Z'
+        end
+
+        entry do
+            name 'Unzoom and equally space all panes'
+            command 'CMD+K CMD+SHIFT+Z'
+        end
+
+    end
+
+
+  notes <<-'END'
+    * Cheatsheet created by [Jacob Reiff](https://github.com/jaacob). [Origami for Sublime Text](https://github.com/SublimeText/Origami) is available via [Package Control](https://sublime.wbond.net/).
+  END
+end
