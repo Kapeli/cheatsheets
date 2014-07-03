d7358537314d773e6a05070e9ce2dc672593e9fe
kidpixo <kidpixo@gmail.com>
2014-07-03 13:35:53 +0200

    Added SQLite.rb cheatsheet

---
+++ cheatsheets/SQLite.rb
@@ -0,0 +1,299 @@
+cheatsheet do
+  title 'SQLite Cheatsheet'
+  docset_file_name 'SQLite'
+  keyword 'sqlite'
+  source_url 'http://cheat.kapeli.com'
+
+  
+  category do
+      id 'sqlite3 Meta Commands'
+      entry do
+          command '.backup ?DB? FILE'
+          name 'Backup DB (default "­mai­n") to FILE'
+      end
+      entry do
+          command '.bail ON|OFF'
+          name 'Stop after hitting an error. Default OFF'
+      end
+      entry do
+          command '.databases'
+          name 'List names and files of attached databases'
+      end
+      entry do
+          command '.dump ?TABLE? ...'
+          name 'Dump the database in an SQL text format.'
+      end
+      entry do
+          command '.echo ON|OFF'
+          name 'Turn command echo on or off'
+      end
+      entry do
+          command '.exit'
+          name 'Exit this program'
+      end
+      entry do
+          command '.explain ?ON|OFF?'
+          name 'Turn output mode suitable for EXPLAIN on or off.'
+      end
+      entry do
+          command '.header(s) ON|OFF'
+          name 'Turn display of headers on or off'
+      end
+      entry do
+          command '.help'
+          name 'Shows this inform­ation.'
+      end
+      entry do
+          command '.import FILE TABLE'
+          name 'Import data from FILE into TABLE'
+      end
+      entry do
+          command '.indices ?TABLE?'
+          name 'Show names of all indices'
+      end
+      entry do
+          command '.load FILE ?ENTRY?'
+          name 'Load an extension library'
+      end
+      entry do
+          command '.log FILE|off'
+          name 'Turn logging on or off. FILE can be stderr­/stdout'
+      end
+      entry do
+          command '.mode MODE ?TABLE?'
+          name 'Set output mode where MODE is one of: csv, column, html, insert, line, list, tabs, tcl'
+      end
+      entry do
+          command '.nullvalue STRING'
+          name 'Print STRING in place of NULL values'
+      end
+      entry do
+          command '.output FILENAME'
+          name 'Send output to FILENAME'
+      end
+      entry do
+          command '.output stdout'
+          name 'Send output to the screen'
+      end
+      entry do
+          command '.prompt MAIN CONTINUE'
+          name 'Replace the standard prompts'
+      end
+      entry do
+          command '.quit'
+          name 'Exit this program'
+      end
+      entry do
+          command '.read FILENAME'
+          name 'Execute SQL in FILENAME'
+      end
+      entry do
+          command '.restore ?DB? FILE'
+          name 'Restore content of DB (default "­mai­n") from FILE'
+      end
+      entry do
+          command '.schema ?TABLE?'
+          name 'Show the CREATE statements'
+      end
+      entry do
+          command '.separator STRING'
+          name 'Change separator used by output mode and .import'
+      end
+      entry do
+          command '.show'
+          name 'Show the current values for various settings'
+      end
+      entry do
+          command '.stats ON|OFF'
+          name 'Turn stats on or off'
+      end
+      entry do
+          command '.tables ?TABLE?'
+          name 'List names of tables'
+      end
+      entry do
+          command '.timeout MS'
+          name 'Try opening locked tables for MS millis­econds'
+      end
+      entry do
+          command '.width NUM1 NUM2 ...'
+          name 'Set column widths for "­col­umn­" mode'
+      end
+      entry do
+          command '.timer ON|OFF'
+          name 'Turn the CPU timer measur­ement on or off'
+      end      
+  end
+
+  category do
+      id 'sqlite3 Options'
+      entry do
+          command '-init file'
+          name 'Read and execute commands from file , which can contain a mix of SQL statements and meta-c­omm­ands.'
+      end
+      entry do
+          command '-echo'
+          name 'Print commands before execution.'
+      end
+      entry do
+          command '-[no]h­eader'
+          name 'Turn headers on or off.'
+      end
+      entry do
+          command '-bail'
+          name 'Stop after hitting an error.'
+      end
+      entry do
+          command '-inter­active'
+          name 'Force intera­ctive I/O .'
+      end
+      entry do
+          command '-batch'
+          name 'Force batch I/O .'
+      end
+      entry do
+          command '-column'
+          name 'Query results will be displayed in a table like form, using whitespace characters to separate the columns and align the output.'
+      end
+      entry do
+          command '-csv'
+          name 'Set output mode to CSV (comma separated values).'
+      end
+      entry do
+          command '-html'
+          name 'Query results will be output as simple HTML tables.'
+      end
+      entry do
+          command '-line'
+          name 'Query results will be displayed with one value per line, rows separated by a blank line. Designed to be easily parsed by scripts or other programs'
+      end
+      entry do
+          command '-list'
+          name 'Query results will be displayed with the separator (|, by default) character between each field value. The default.'
+      end
+      entry do
+          command '-separator separator'
+          name 'Set output field separator. Default is "|".'
+      end
+      entry do
+          command '-stats'
+          name 'Print memory stats before each finalize.'
+      end
+      entry do
+          command '-nullvalue string'
+          name 'Set string used to represent NULL values. Default is '' (empty string).'
+      end
+      entry do
+          command '-version'
+          name 'Show SQLite version.'
+      end
+      entry do
+          command '-vfs name'
+          name 'Use name as the default VFS .'
+      end
+      entry do
+          command '-help'
+          name 'Show help on options and exit.'
+      end
+  end
+  
+
+  category do
+      id 'sqlite3 See Also'
+      
+      entry do
+          command 'http:/­/ww­w.s­qli­te.org/'
+          notes 'The sqlite­3-doc package.'
+      end
+      
+  end
+
+  notes <<-'END'
+  Original cheatsheet from [sqlite3 Cheat Sheet by richardjh - Cheatography.com: Cheat Sheets For Every Occasion](http://www.cheatography.com/richardjh/cheat-sheets/sqlite3/)
+  <br />
+  Create the docset:<br />
+  Copy the text from [sqlite3 Cheat Sheet by richardjh](http://www.cheatography.com/richardjh/cheat-sheets/sqlite3/) and then substitute with your text editor of choice.
+
+  **Section "sqlite3 Meta Commands" :**
+
+  **description**
+  <pre><code>
+	select patern
+  <br />
+  replace pattern
+  </code></pre>
+
+  **change all lines not starting with "." = names**
+  <pre><code>
+  ^([^\.].*$)
+  <br />
+  name '$1'\nend
+  </code></pre>
+
+
+  change all lines starting with "." = commands
+  <pre><code>
+  ^(\..*$)
+  <br />
+  entry do\ncommand '$1'
+  </code></pre>
+
+  **some indentation**
+  <pre><code>
+  <br />
+  ^([^e])
+      $1
+  </code></pre>
+  
+  **pipe quotation was crashing ruby**
+  <pre><code>
+  '|'
+  <br />
+  "|"
+  </code></pre>
+
+  **Section "sqlite3 Options" :**
+
+  **description**
+  <pre><code>
+  select patern
+  <br />
+  replace pattern
+  </code></pre>
+
+  **change all lines not starting with "-" = names**
+  <pre><code>
+  ^([^-].*$)
+  <br />
+  name '$1'\nend
+  </code></pre>
+
+  **change all lines starting with "-" = commands**
+  <pre><code>
+  ^(-.*$)
+  <br />
+  entry do\ncommand '$1'
+    </code></pre>
+
+  **some indentation**
+  <pre><code>
+  ^([^e])
+  <br />
+      $1
+  </code></pre>
+	 
+  **pipe quotation was crashing ruby**
+  <pre><code>
+  '|'
+  <br />
+  "|"
+  </code></pre>
+  
+  **Section 'sqlite3 See Also':**
+  done by hand.
+  
+  Happy cheating!  
+  
+  END
+  
+end
