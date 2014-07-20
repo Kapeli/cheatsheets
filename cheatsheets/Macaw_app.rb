78e020b7b2f7518eef0f1ebb8b9d0c25c682414c
Nicolas Genty <nicolas.genty@gmail.com>
2014-07-20 03:11:16 +0200

    Create Macaw_app.rb
    Macaw app cheatsheet [Macaw app](http://macaw.co/) from the official documentation.

---
+++ cheatsheets/Macaw_app.rb
@@ -0,0 +1,410 @@
+cheatsheet do
+  title 'Macaw'
+  docset_file_name 'Macaw_app'
+  keyword 'macaw'
+  source_url 'http://cheat.kapeli.com'
+
+  category do
+    id 'File'
+
+    entry do
+      command '⇧⌘N'
+      name 'New Project'
+    end
+
+    entry do
+      command '⌘N'
+      name 'New Page…'
+    end
+
+    entry do
+      command '⌘O'
+      name 'Open…'
+    end
+
+    entry do
+      command '⌘S'
+      name 'Save'
+    end
+
+    entry do
+      command '⇧⌘S'
+      name 'Save As…'
+    end
+
+    entry do
+      command '⌥⌘S'
+      name 'Save All'
+    end
+
+    entry do
+      command '⌘I'
+      name 'Import to Canvas…'
+    end
+
+    entry do
+      command '⇧⌘I'
+      name 'Import to Library…'
+    end
+
+    entry do
+      command '⌘P'
+      name 'Publish Page'
+    end
+
+    entry do
+      command '⌥⌘P'
+      name 'Publish Project'
+    end
+
+    entry do
+      command '⇧⌘P'
+      name 'Publish Settings…'
+    end
+
+  end
+
+  category do
+    id 'Edit'
+
+    entry do
+      command '⌘Z'
+      name 'Undo'
+    end
+
+    entry do
+      command '⇧⌘Z'
+      name 'Redo'
+    end
+
+    entry do
+      command '⌘X'
+      name 'Cut'
+    end
+
+    entry do
+      command '⌘C'
+      name 'Copy'
+    end
+
+    entry do
+      command '⌥⌘C'
+      name 'Copy Visual Properties'
+    end
+
+    entry do
+      command '⌘V'
+      name 'Paste'
+    end
+
+    entry do
+      command '⌥⌘V'
+      name 'Paste Visual Properties'
+    end
+
+    entry do
+      command '⌘D'
+      name 'Duplicate'
+    end
+
+    entry do
+      command '⌘['
+      name 'Move Backwards'
+    end
+
+    entry do
+      command '⇧⌘['
+      name 'Send to Back'
+    end
+
+    entry do
+      command '⌘]'
+      name 'Move Forward'
+    end
+
+    entry do
+      command '⇧⌘]'
+      name 'Send to Front'
+    end
+
+    entry do
+      command '⌘R'
+      name 'Rename'
+    end
+
+    entry do
+      id 'Select'
+    end
+
+    entry do
+      command '⌘A'
+      name 'Select All'
+    end
+
+    entry do
+      command '⌥['
+      name 'Select Element Below'
+    end
+
+    entry do
+      command '⌥]'
+      name 'Select Element Above'
+    end
+  end
+
+  category do
+    id 'View'
+
+    entry do
+      command '⌘Y'
+      name 'Toggle Outline Mode'
+    end
+
+    entry do
+      command '⌘W'
+      name 'Toggle Wireframe Mode'
+    end
+
+    entry do
+      command '⌥⌘0'
+      name 'Center Canvas'
+    end
+
+    entry do
+      command '⌘;'
+      name 'Toggle Grid'
+    end
+
+    entry do
+      command '⌘U'
+      name 'Toggle Snap to Objects'
+    end
+
+    entry do
+      command 'Tab'
+      name 'Toggle Palettes'
+    end
+
+    entry do
+      command '⇧['
+      name 'View Lower Breakpoint'
+    end
+
+    entry do
+      command '⇧]'
+      name 'View Higher Breakpoint'
+    end
+
+    entry do
+      command '⇧\'
+      name 'Toggle to Default Breakpoint'
+    end
+  end
+
+  category do
+    id 'Tools'
+
+    entry do
+      command 'V'
+      name 'Selection Tool'
+    end
+
+    entry do
+      command 'A'
+      name 'Direct Select Tool'
+    end
+
+    entry do
+      command 'T'
+      name 'Text Tool'
+    end
+
+    entry do
+      command 'R'
+      name 'Element Tool'
+    end
+
+    entry do
+      command 'G'
+      name 'Container Tool'
+    end
+
+    entry do
+      command 'B'
+      name 'Button Tool'
+    end
+
+    entry do
+      command 'N'
+      name 'Input Tool'
+    end
+
+    entry do
+      command 'M'
+      name 'Embed Tool'
+    end
+
+    entry do
+      command 'H'
+      name 'Hand Tool'
+    end
+
+    entry do
+      command 'I'
+      name 'Eyedropper Tool'
+    end
+  end
+
+  category do
+    id 'Window'
+
+    entry do
+      command '⌥I'
+      name 'Show Inspector'
+    end
+
+    entry do
+      command '⌥O'
+      name 'Show Outline'
+    end
+
+    entry do
+      command '⌥L'
+      name 'Show Library'
+    end
+
+    entry do
+      command 'P'
+      name 'Show Pages'
+    end
+
+    entry do
+      command 'C'
+      name 'Show Color Picker'
+    end
+
+    entry do
+      command 'S'
+      name 'Show Quick Swatches'
+    end
+
+    entry do
+      command 'F8'
+      name 'Show Scripts'
+    end
+
+    entry do
+      command 'Q'
+      name 'Show Queries'
+    end
+  end
+
+  category do
+    id 'Elements'
+
+    entry do
+      command '⌘G'
+      name 'Tight Group'
+    end
+
+    entry do
+      command '⌥⌘G'
+      name 'Full-Width Group'
+    end
+
+    entry do
+      command '⇧⌘G'
+      name 'Ungroup'
+    end
+
+    entry do
+      command '⌘T'
+      name 'Transform'
+    end
+
+    entry do
+      command '⌘E'
+      name 'Expand'
+    end
+
+    entry do
+      command '⌘F'
+      name 'Fit to Children'
+    end
+
+    # entry do
+    #   command '⌘M'
+    #   name 'Morph…'
+    # end
+
+    entry do
+      command '⌥⌘R'
+      name 'Refresh All Assets'
+    end
+
+    # entry do
+    #   command '⌘3'
+    #   name 'Hide Selected'
+    # end
+
+    # entry do
+    #   command '⌥⌘3'
+    #   name 'Show All'
+    # end
+
+    entry do
+      command '⌘2'
+      name 'Lock Selected'
+    end
+
+    entry do
+      command '⌥⌘2'
+      name 'Unlock All'
+    end
+
+    entry do
+      command '⌘K'
+      name 'Create Component…'
+    end
+
+    entry do
+      command '⌘B'
+      name 'Break Component Reference'
+    end
+
+    entry do
+      command '→'
+      name 'Nudge 1px'
+    end
+
+    entry do
+      command '⇧→'
+      name 'Nudge 10px'
+    end
+
+    entry do
+      command '⌘→'
+      name 'Nudge to Grid'
+    end
+
+    entry do
+      command '⌥→'
+      name 'Pudge 1px'
+    end
+
+    entry do
+      command '⇧⌥→'
+      name 'Pudge 10px'
+    end
+
+    entry do
+      command '⌥⌘→'
+      name 'Pudge to Grid'
+    end
+  end
+
+  notes <<-'END'
+  * Based on the official cheat sheet.
+  * Converted by [Nicolas Genty](https://twitter.com/ngenty)
+  END
+end
