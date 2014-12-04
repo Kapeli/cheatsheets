0032c1142dba3eb3dc0c30f0da081f138b81576c
Esteban Torres <esteban@logn.co>
2014-12-03 22:10:18 -0600

    ASCII Art Japanese cheatsheet

---
+++ cheatsheets/ASCII_Art.rb
@@ -0,0 +1,307 @@
+cheatsheet do
+  title 'ASCII Art'
+  docset_file_name 'ASCII_Art'
+  keyword 'asciiart'
+  source_url 'http://cheat.kapeli.com'
+  introduction "Awesome `ASCII` art cheatsheet for some `Japanese Emoticons & Emoji`.<br/>
+  <h2>Contact</h2>
+  [![Twitter: @esttorhe](http://img.shields.io/badge/Twitter%20-%40esttorhe-blue.svg?style=flat)](https://twitter.com/esttorhe)"
+
+  category do
+  id 'Table Flip Emoji & Emoticons (╯°□°）╯︵ ┻━┻'
+
+    entry do
+      command '(╯°□°）╯︵ ┻━┻'
+      name    'Default table flip'
+    end
+
+    entry do
+      command '/( .□.)\ ︵╰(゜益゜)╯︵ /(.□. /)'
+      name 'People flip'
+    end
+
+    entry do
+      command '(╯°Д°）╯︵/(.□ . \)'
+      name 'Guy flip'
+    end
+
+    entry do
+      command '(ノಠ ∩ಠ)ノ彡( \o°o)\ '
+      name 'Indignant guy flip'
+    end
+
+    entry do
+      command '(╯°□°)╯︵ ʞooqǝɔɐℲ'
+      name 'Facebook flip'
+    end
+
+    entry do
+      command 'ʕノ•ᴥ•ʔノ ︵ ┻━┻'
+      name 'Bear table flip'
+    end
+
+    entry do
+      command '(/¯◡ ‿ ◡)/¯ ~ ┻━┻'
+      name 'Relaxed table flip'
+    end
+
+    entry do
+      command '(ノ-_-)ノ ~┻━┻ (/o＼)'
+      name 'Throwing table flip'
+    end
+
+    entry do
+      command '(ノಠ益ಠ)ノ彡┻━┻'
+      name 'Angry table flip'
+    end
+
+    entry do
+      command 'ノ┬─┬ノ ︵ ( \o°o)\ '
+      name 'Reverse table flip'
+    end
+  end
+
+  category do
+    id 'Troll Emoticons ༼ ༎ຶ ෴ ༎ຶ༽'
+
+    entry do
+      command 'ヽ༼ ಠ益ಠ ༽ﾉ'
+      name 'Angry troll'
+    end
+
+    entry do
+      command '(((༼•̫͡•༽)))'
+      name 'Silly ghost troll'
+    end
+
+    entry do
+      command 'ヽ༼ຈل͜ຈ༽ﾉ'
+      name 'Clueless troll'
+    end
+
+    entry do
+      command '༼(⁽͇ˊ̑⁾ ἴृ ⁽ˋ̑⁾͇)༽'
+      name 'Cute troll'
+    end
+
+    entry do
+      command '(ી(΄◞ิ౪◟ิ‵)ʃ)'
+      name 'Jolly troll'
+    end
+
+    entry do
+      command '༼ ͒ ͓ ͒༽'
+      name 'Speechless troll'
+    end
+
+    entry do
+      command '༼ ͒ ̶ ͒༽'
+      name "Neutral troll"
+    end
+
+    entry do
+      command '༼ ༎ຶ ෴ ༎ຶ༽'
+      name 'Crying troll'
+    end
+
+    entry do
+      command '༼ ༏༏ີཻ༾ﾍ ༏༏ີཻ༾༾༽༽'
+      name 'Crying troll 2'
+    end
+
+    entry do
+      command '༼･ิิɷ･ิ༽'
+      name 'Cute troll'
+      notes 'The characters used for the eyes are not correctly rendered in `Dash`, thus the art might not look 100% accurate.'
+    end
+  end
+
+  category do
+    id 'Dead Emoticons & Emojis (✖╭╮✖)'
+
+    entry do
+      command '(✖╭╮✖)'
+      name 'Standard dead face'
+    end
+
+    entry do
+      command '✖‿✖'
+      name 'Happy dead face'
+    end
+
+    entry do
+      command '٩(×̯×)۶'
+      name 'Dead guy'
+    end
+  end
+
+  category do
+    id 'Crying Emoticons & Emojis ｡ﾟ(ﾟ´(00)`ﾟ)ﾟ｡'
+
+    entry do
+      command '(´°̥̥̥̥̥̥̥̥ω°̥̥̥̥̥̥̥̥｀)'
+      name 'Big tears'
+    end
+
+    entry do
+      command '(*´°̥̥̥̥̥̥̥̥﹏°̥̥̥̥̥̥̥̥ )人(´°̥̥̥̥̥̥̥̥ω°̥̥̥̥̥̥̥̥｀)'
+      name 'Big tears bros'
+    end
+
+    entry do
+      command 'ლ(ٱ٥ٱლ)'
+      name 'Why? oh god why? tears'
+    end
+
+    entry do
+      command '( •́દ•̩̥̀ )'
+      name 'Single tear'
+    end
+
+    entry do
+      command '̨(༎ິ῀̫ ༎ິ ̨ )͞˭̳̳̳˭̳̳̳ˍ̿̿ˍ̿ˌ˳ˏ̇⋅∴༣'
+      name 'Runaway tears'
+    end
+
+    entry do
+      command '(　-̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥᷄◞ω◟-̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥᷅ )'
+      name 'Waterfall tears'
+      notes 'The tears are **WAY** longer than they appear on the preview here. `Dash` can\'t render them '
+    end
+  end
+
+  category do
+    id 'Kissing Emoticons & Emojis ლ(|||⌒εー|||)ლ'
+
+    entry do
+      command '(╯3╰)'
+      name 'Eyes closed kiss'
+    end
+
+    entry do
+      command '( ˘ ³˘)♥'
+      name 'Blow me a kiss'
+    end
+
+    entry do
+      command '( c//”-}{-*\x)'
+      name 'Two people kissing'
+    end
+
+    entry do
+      command 'ლ(|||⌒εー|||)ლ'
+      name 'Wink kiss'
+    end
+  end
+
+  category do
+    id 'Hugging Emoticons & Emojis (づ￣ ³￣)づ'
+
+    entry do
+      command '(づ｡◕‿‿◕｡)づ'
+      name 'Cute hug'
+    end
+
+    entry do
+      command '(づ￣ ³￣)づ'
+      name 'XOXO'
+    end
+
+    entry do
+      command '(っ˘̩╭╮˘̩)っ'
+      name 'Sad hug'
+    end
+  end
+
+  category do
+    id 'Thinking Emoticons & Emojis (;¬_¬)'
+
+    entry do
+      command '(;¬_¬)'
+      name 'I\'m judging you'
+    end
+
+    entry do
+      command '（；¬＿¬)'
+      name 'I\'m severely judging you'
+    end
+
+    entry do
+      command '( ͡° ͜ʖ ͡°)'
+      name 'How you doing?'
+    end
+
+    entry do
+      command '(－‸ლ)'
+      name 'Facepalm'
+    end
+  end
+
+  category do
+    id 'Hurt Emoticons & Emojis （。_°☆＼(- – )'
+
+    entry do
+      command '（。_°☆＼(- – )'
+      name 'Bitch slap'
+    end
+
+    entry do
+      command '☆⌒(＞。≪)'
+      name 'Punch'
+    end
+
+    entry do
+      command '☆￣(＞。☆)'
+      name 'Punch and black eye'
+    end
+  end
+
+  category do
+    id 'Surprised Japanese Emoticons ∑(;°Д°)'
+
+    entry do
+      command '(((( ;°Д°))))'
+      name 'Big surprise'
+    end
+
+    entry do
+      command '((((；゜Д゜)))'
+      name 'Worried/surprise'
+    end
+
+    entry do
+      command '∑(O_O；)'
+      name 'WHAT!? Surprise'
+    end
+
+    entry do
+      command '━Σ(ﾟДﾟ|||)━'
+      name 'HOLY @#$%@%!!'
+    end
+
+    entry do
+      command '⊙▂⊙'
+      name 'I\'m dead'
+    end
+
+    entry do
+      command 'w(°ｏ°)w'
+      name 'Wow'
+    end
+
+    entry do
+      command '∑(;°Д°)'
+      name 'FUUUUUUUCK'
+    end
+
+    entry do
+      command '(´⊙ω⊙`)！'
+      name 'Shit!'
+    end
+  end
+
+  notes 'Information obtained from:
+
+* http://hexascii.com/'
+
+end
