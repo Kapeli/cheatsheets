4a43dec5b3cd434b5be43f15dca58f4bb71fdcd3
Kapeli <support@kapeli.com>
2014-12-11 14:27:46 +0200

    CSS Device Media Queries fixes/changes

--- cheatsheets/CSS_Device_Media_Queries.rb
+++ cheatsheets/CSS_Device_Media_Queries.rb
@@ -1,30 +1,42 @@
 cheatsheet do
   title 'CSS Device Media Queries'
-  docset_file_name 'CSS_Device_Media_queries'
-  keyword '@media'
+  docset_file_name 'CSS_Device_Media_Queries'
+  keyword 'media'
+  source_url 'http://cheat.kapeli.com'
 
-  # A cheat sheet must consist of categories
   category do
-    id 'Smartphones'
+    id 'Phones'
 
     entry do
-      command '@media only screen and (min-device-width: 320px) and (max-device-width: 480px)'
-      name 'portrait and landscape'
+      notes '
+      ```css
+      @media only screen and (min-device-width: 320px) and (max-device-width: 480px)
+      ```'
+      name 'Portrait and landscape phones'
     end
 
     entry do
-      command '@media only screen and (min-width: 321px)'
-      name 'landscape'
+      notes '
+      ```css
+      @media only screen and (min-width: 321px)
+      ```'
+      name 'Landscape phones'
     end
 
     entry do
-      command '@media only screen and (max-width: 320px)'
-      name 'portrait'
+      notes '
+      ```css
+      @media only screen and (max-width: 320px)
+      ```'
+      name 'Portrait phones'
     end
 
     entry do
-      command '@media only screen and (-webkit-min-device-pixel-ratio: 1.5), ' +
-              'only screen (min-device-pixel-ratio: 1.5)'
+      notes '
+      ```css
+      @media only screen and (-webkit-min-device-pixel-ratio: 1.5), ' +
+              'only screen (min-device-pixel-ratio: 1.5)
+      ```'
       name 'iPhone 4'
     end
   end
@@ -33,18 +45,27 @@ cheatsheet do
     id 'iPads (Tablets)'
 
     entry do
-      command '@media only screen and (min-device-width: 768px) and (max-device-width: 1024px)'
-      name 'portrait and landscape'
+      notes '
+      ```css
+      @media only screen and (min-device-width: 768px) and (max-device-width: 1024px)
+      ```'
+      name 'Portrait and landscape tablets'
     end
 
     entry do
-      command '@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: landscape)'
-      name 'landscape'
+      notes '
+      ```css
+      @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: landscape)
+      ```'
+      name 'Landscape tablets'
     end
 
     entry do
-      command '@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: portrait)'
-      name 'portrait'
+      notes '
+      ```css
+      @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: portrait)
+      ```'
+      name 'Portrait tablets'
     end
   end
 
@@ -52,17 +73,23 @@ cheatsheet do
     id 'Other'
 
     entry do
-      command '@media only screen and (min-width: 1224px)'
-      name 'desktops and laptops'
+      notes '
+      ```css
+      @media only screen and (min-width: 1224px)
+      ```'
+      name 'Desktops and laptops'
     end
 
     entry do
-      command '@media only screen and (min-width: 1824px)'
-      name 'large screens'
+      notes '
+      ```css
+      @media only screen and (min-width: 1824px)
+      ```'
+      name 'Large screens'
     end
   end
 
-  notes "* Based on blog post from [css-tricks.com](http://css-tricks.com/snippets/css/media-queries-for-standard-devices/)
+  notes "* Based on a blog post from [css-tricks.com](http://css-tricks.com/snippets/css/media-queries-for-standard-devices/)
 * Converted by [@joelash](https://twitter.com/joelash)"
 
 end
