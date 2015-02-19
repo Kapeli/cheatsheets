b506e4166f41ba9f561e7e2f11e4c353183f7f3d
Joe Pasqualetti <public_github@joepasq.com>
2015-02-18 19:20:40 -0500

    Update git delete remote branch in Remote section. Add track a repo command.
    - Tracking a remote repo is useful when getting in sync with the root repo that is the parent of a fork.

--- cheatsheets/Git.rb
+++ cheatsheets/Git.rb
@@ -263,8 +263,21 @@ cheatsheet do
       name 'Delete a branch on the remote'
       notes "
       ```
+      git push <remote> --delete <branch>
+      # alternatively
       git push <remote> :<branch>
       ```"
+    end
+
+    entry do
+      name 'Track a remote repository'
+      notes "
+      ```
+      git remote add --track <remote-branch> <remote> <project.git>
+      ```
+      Ex: pull updates from the root of a forked repo
+      via http://bradlyfeeley.com/2008/09/03/update-a-github-fork-from-the-original-repo/
+      "
     end
     
     entry do
