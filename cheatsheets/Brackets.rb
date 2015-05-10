c94ad888d5c5e6cc94519d0ac88c950dc77b1077
David Waterston <david@davidwaterston.com>
2015-05-10 18:13:10 +0100

    Add cheatsheet for Brackets editor
    Add cheatsheet for Brackets editor for Mac, version 1.3

---
+++ cheatsheets/Brackets.rb
@@ -0,0 +1,412 @@
+cheatsheet do
+    title 'Brackets'
+    docset_file_name 'Brackets'
+    keyword 'brackets'
+    source_url 'http://cheat.kapeli.com'
+
+  
+  category do
+    id 'View / Window Manipulation'
+    entry do
+      command 'CMD+SHIFT+H'
+      name 'Toggle sidebar'
+    end
+    entry do
+      command 'CMD+M'
+      name 'Minimize'
+    end
+  end
+  
+  
+  category do
+    id 'File and Folders'
+    entry do
+      command 'CMD+N'
+      name 'New file'
+    end
+    entry do
+      command 'CMD+O'
+      name 'Open file'
+    end
+    entry do
+      command 'CMD+ALT+O'
+      name 'Open folder'
+    end
+    entry do
+      command 'CMD+W'
+      name 'Close file'
+    end
+    entry do
+      command 'CMD+SHIFT+W'
+      name 'Close all files'
+    end
+  end
+  
+  
+  category do
+    id 'File Navigation'
+    entry do
+      command 'CMD+SHIFT+O'
+      name 'Quick open'
+    end
+    entry do
+      command 'CMD+L'
+      name 'Go to line'
+    end
+    entry do
+      command 'CMD+T'
+      name 'Quick find definition'
+    end
+    entry do
+      command 'CMD+J'
+      name 'Jump to definition'
+    end
+    entry do
+      command 'CMD+`'
+      name 'Goto first problem'
+    end
+    entry do
+      command 'CMD+SHIFT+]'
+      name 'Next document'
+    end
+    entry do
+      command 'CMD+SHIFT+['
+      name 'Previous document'
+    end
+    entry do
+      command 'CMD+E'
+      name 'Toggle quick edit'
+    end
+    entry do
+      command 'ALT+Arrow Up'
+      name 'Previous match'
+    end
+    entry do
+      command 'ALT+Arrow Down'
+      name 'Next match'
+    end
+    entry do
+      command 'CMD+ALT+N'
+      name 'New rule'
+    end
+    entry do
+      command 'CMD+FN+Arrow Left'
+      name 'Goto file start'
+    end
+    entry do
+      command 'CMD+FN+Arrow Right'
+      name 'Goto file end'
+    end
+    entry do
+      command 'FN+Arrow Up'
+      name 'Page up'
+    end
+    entry do
+      command 'FN+Arrow Down'
+      name 'Page down'
+    end
+  end
+  
+  
+  category do
+    id 'Find'
+    entry do
+      command 'CMD+F'
+      name 'Find in file'
+    end
+    entry do
+      command 'CMD+SHIFT+F'
+      name 'Find in project'
+    end
+    entry do
+      command 'CMD+G'
+      name 'Find next'
+    end
+    entry do
+      command 'CMD+SHIFT+G'
+      name 'Find previous'
+    end
+    entry do
+      command 'CMD+CTRL+G'
+      name 'Find all and select'
+    end
+    entry do
+      command 'CMD+B'
+      name 'Add next match to selection'
+    end
+    entry do
+      command 'CMD+SHIFT+B'
+      name 'Skip and add next match to selection'
+    end
+  end
+
+  
+  category do
+    id 'Replace'
+    entry do
+      command 'CMD+ALT+F'
+      name 'Replace'
+    end
+    entry do
+      command 'CMD+ALT+SHIFT+F'
+      name 'Replace in project'
+    end
+  end
+  
+  
+  category do
+    id 'Folding'
+    entry do
+      command 'ALT+1'
+      name 'Collapse all'
+    end
+    entry do
+      command 'ALT+SHIFT+1'
+      name 'Expand all'
+    end
+    entry do
+      command 'CMD+ALT+['
+      name 'Collapse current'
+    end
+    entry do
+      command 'CMD+ALT+]'
+      name 'Expand current'
+    end
+  end
+  
+  
+  category do
+    id 'Basic Editing'
+    entry do
+      command 'CMD+C'
+      name 'Copy'
+    end
+    entry do
+      command 'CMD+X'
+      name 'Cut'
+    end
+    entry do
+      command 'CMD+V'
+      name 'Paste'
+    end
+    entry do
+      command 'CMD+Z'
+      name 'Undo'
+    end
+    entry do
+      command 'CMD+SHIFT+Z'
+      name 'Redo'
+    end
+  end
+  
+  
+  category do
+    id 'Line Manipulation'
+    entry do
+      command 'CMD+]'
+      name 'Indent current line or selection'
+    end
+    entry do
+      command 'CMD+['
+      name 'Outdent current line or selection'
+    end
+    entry do
+      command 'CMD+Return'
+      name 'Insert newline after current line or selection'
+    end
+    entry do
+      command 'CMD+SHIFT+Return'
+      name 'Insert newline before current line or selection'
+    end
+    entry do
+      command 'CTRL+SHIFT+D'
+      name 'Delete current line or selection'
+    end
+    entry do
+      command 'CMD+Backspace'
+      name 'Delete to line start'
+    end
+    entry do
+      command 'CMD+Delete'
+      name 'Delete to line end'
+    end
+    entry do
+      command 'CMD+CTRL+Arrow Up'
+      name 'Move current line or selection up'
+    end
+    entry do
+      command 'CMD+CTRL+Arrow Down'
+      name 'Move current line or selection down'
+    end
+    entry do
+      command 'CMD+D'
+      name 'Duplicate current line or selection'
+    end
+    entry do
+      command 'CMD+ALT+L'
+      name 'Split selection into lines'
+    end
+    entry do
+      command 'CMD+/'
+      name 'Toggle line comment'
+    end
+    entry do
+      command 'CMD+ALT+/'
+      name 'Toggle block comment'
+    end
+  end
+  
+  
+  category do
+    id 'Word Manipulation'
+    entry do
+      command 'CTRL+T'
+      name 'Transpose characters either side of the cursor'
+    end
+    entry do
+      command 'ALT+Backspace'
+      name 'Delete text to beginning of word'
+    end
+    entry do
+      command 'ALT+Delete'
+      name 'Delete text to end of word'
+    end
+  end
+  
+  
+  category do
+    id 'Cursors'
+    entry do
+      command 'ALT+SHIFT+Arrow Down'
+      name 'Add cursor to next line'
+    end
+    entry do
+      command 'ALT+SHIFT+Arrow Up'
+      name 'Add cursor to previous line'
+    end
+  end
+  
+  
+  category do
+    id 'Selection'
+    entry do
+      command 'CMD+A'
+      name 'Select all'
+    end
+    entry do
+      command 'CTRL+L'
+      name 'Select current line'
+    end
+    entry do
+      command 'ALT+SHIFT+Arrow Left'
+      name 'Select to beginning of word'
+    end
+    entry do
+      command 'ALT+SHIFT+Arrow Right'
+      name 'Select to end of word'
+    end
+    entry do
+      command 'CMD+SHIFT+Arrow Left'
+      name 'Select to first character of line'
+    end
+    entry do
+      command 'CMD+SHIFT+Arrow Right'
+      name 'Select to last character of line'
+    end
+    entry do
+      command 'CMD+SHIFT+Arrow Up'
+      name 'Select to beginning of document'
+    end
+    entry do
+      command 'CMD+SHIFT+Arrow Down'
+      name 'Select to end of document'
+    end
+    entry do
+      command 'CMD+U'
+      name 'Undo selection'
+    end
+    entry do
+      command 'CMD+SHIFT+U'
+      name 'Redo selection'
+    end
+  end
+  
+  
+  category do
+    id 'Hints'
+    entry do
+      command 'CTRL+Space'
+      name 'Show code hints'
+    end
+    entry do
+      command 'CTRL+SHIFT+Space'
+      name 'Show parameter hint'
+    end
+  end
+  
+  
+  category do
+    id 'Live Preview'
+    entry do
+      command 'CMD+ALT+P'
+      name 'Enable live preview'
+    end
+    entry do
+      command 'CMD+SHIFT+C'
+      name 'Toggle live preview highlight'
+    end
+    entry do
+      command 'CMD+SHIFT+R'
+      name 'Force reload of live preview'
+    end
+  end
+
+
+  category do
+    id 'Debugging'
+    entry do
+      command 'CMD+ALT+I'
+      name 'Show developer tools'
+    end
+    entry do
+      command 'CMD+R'
+      name 'Reload with extensions'
+    end
+    entry do
+      command 'CMD+CTRL-R'
+      name 'Reload without extensions'
+    end
+  end
+  
+  
+  category do
+    id 'Miscellaneous'
+    entry do
+      command 'F1'
+      name 'Help'
+    end
+    entry do
+      command 'F2'
+      name 'Rename file'
+    end
+    entry do
+      command 'CMD+Plus'
+      name 'Increase font size'
+    end
+    entry do
+      command 'CMD+Minus'
+      name 'Decrease font size'
+    end
+    entry do
+      command 'CMD+0'
+      name 'Restore font size to default'
+    end
+
+  
+  end
+  
+  
+  notes <<-'END'
+    * Created by [David Waterston](https://github.com/davidwaterston).
+  END
+end
