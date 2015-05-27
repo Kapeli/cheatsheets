fbeff4502e4f04d2bc122570a5033f33dcff7881
Kapeli <support@kapeli.com>
2015-05-27 13:36:32 +0300

    Unix File Permissions small changes

--- cheatsheets/Unix_File_Permissions.rb
+++ cheatsheets/Unix_File_Permissions.rb
@@ -83,31 +83,31 @@ cheatsheet do
 
     entry do
       command 'l'
-      name 'Symbolic Link'
+      name 'Symbolic link'
       notes 'Example: ```lrwxrwxrwx 1 root root           4 1 January 00:00 rtc -> rtc0```'
     end
 
     entry do
       command 'b'
-      name 'Block Special Device'
+      name 'Block special device'
       notes 'Example: ```brw-rw---- 1 root disk      1   0 1 January 00:00  ram0```'
     end
 
     entry do
       command 'c'
-      name 'Character Device'
+      name 'Character device'
       notes 'Example: ```crw-rw-rw- 1 root root      1   3 1 January 00:00  null```'
     end
 
     entry do
       command 's'
-      name 'Unix Socket'
+      name 'Unix socket'
       notes 'Example: ```srw-rw-rw- 1 root       root          0 1 January 00:00  acpid.socket```'
     end
 
     entry do
       command 'p'
-      name 'Named Pipe'
+      name 'Named pipe'
       notes 'Example: ```prw-r--r-- 1 root root 0 1 January 00:00  pipe```'
     end
   end
@@ -115,11 +115,11 @@ cheatsheet do
   category do
     id 'Special Mode Bits'
     entry do
-      name "setuid (Set User ID)"
+      name "`setuid` (Set User ID)"
       notes "When the setuid permission is set on an executable file, a process that runs this file is granted access based on the owner of the file (usually root) \n\nThis special permission allows a user to access files and directories that are normally only available to the owner.  \n\n Example: The setuid permission on the passwd command makes it possible for a user to change passwords, assuming the permissions of the root ID: \n\n``-r-sr-sr-x   3 root     sys       104580 Sep 16 12:02 /usr/bin/passwd``"
     end
     entry do
-      name "setgid (Set Group ID)"
+      name "`setgid` (Set Group ID)"
       notes "The set-group identification (setgid) permission is similar to setuid, except that the process's effective group ID (GID) is changed to the group owner of the file. \n\n Example: The /usr/bin/mail command has setgid permissions: \n\n``-r-x--s--x   1 root     mail       63628 Sep 16 12:01 /usr/bin/mail``"
     end
     entry do
@@ -128,27 +128,27 @@ cheatsheet do
     end
     entry do
       command '--S------'
-      name 'setuid is set, but user (owner) execute permission is not set.'
+      name 'setuid is set, but user (owner) execute permission is not set'
     end
     entry do
       command '--s------'
-      name 'setuid and user execute persmission are both set.'
+      name 'setuid and user execute persmission are both set'
     end
     entry do
       command '-----S---'
-      name 'setgid is set, but group execute permission is not set.'
+      name 'setgid is set, but group execute permission is not set'
     end
     entry do
       command '-----s---'
-      name 'setgid and group execute permission are both set.'
+      name 'setgid and group execute permission are both set'
     end
     entry do
       command '--------T'
-      name 'sticky bit is set, bot other execute permission is not set.'
+      name 'sticky bit is set, bot other execute permission is not set'
     end
     entry do
       command '--------t'
-      name 'sticky bit and other execute permission are both set.'
+      name 'sticky bit and other execute permission are both set'
     end
     entry do
       notes 'Note: Use setuid, and setgid with caution, incorrect use of setuid, and setgid can pose a security risk.'
