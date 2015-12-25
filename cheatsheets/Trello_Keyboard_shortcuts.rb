06e6e4d5915053b3a9225815c94ecccf7a5da2f6
Chris Ward <chriswhward@gmail.com>
2015-12-25 14:21:51 +0100

    Key capitalisation and removing or on certain keys, not really necessary. Code formatted keys inline in notes

--- cheatsheets/Trello_Keyboard_shortcuts.rb
+++ cheatsheets/Trello_Keyboard_shortcuts.rb
@@ -7,65 +7,65 @@ cheatsheet do
 	category do
 		id 'Trello Keyboard Shortcuts'
 		entry do
-			command '↑ or k'
+			command 'Arrow Up'
 			name 'Select the card above the current card.'
 		end
 		entry do
-			command '↓ or j'
+			command 'Arrow Down'
 			name 'Select the card below the current card.'
 		end
 		entry do
-			command '←'
+			command 'Arrow Left'
 			name 'Select the card to the left of the current card.'
 		end
 		entry do
-			command '→'
+			command 'Arrow Right'
 			name 'Select the card to the right of the current card.'
 		end
 
 		entry do
-			command 'b'
-			name 'Pressing “b” opens the boards menu in the header.'
+			command 'B'
+			name 'Pressing `B` opens the boards menu in the header.'
 		end
 		entry do
 			command '/'
-			name 'Pressing “/” puts the cursor in the search box in the header.'
+			name 'Pressing `/` puts the cursor in the search box in the header.'
 		end
 		entry do
-			command 'c'
-			name 'Pressing “c” will archive a card.'
+			command 'C'
+			name 'Pressing `C` will archive a card.'
 		end
 		entry do
-			command 'd'
-			name 'Pressing “d” will open the due date picker for a card.'
+			command 'D'
+			name 'Pressing `D` will open the due date picker for a card.'
 		end
 		entry do
-			command 'e'
-			name 'If hovering over a card, pressing “e” will open quick edit mode, which lets you quickly edit the title and other card attributes.'
+			command 'E'
+			name 'If hovering over a card, pressing `E` will open quick edit mode, which lets you quickly edit the title and other card attributes.'
 		end
 		entry do
-			command 'esc'
-			name 'Pressing “esc” will close an open dialog window or pop-over or cancel edits and comments you are composing.'
+			command 'Esc'
+			name 'Pressing `esc` will close an open dialog window or pop-over or cancel edits and comments you are composing.'
 		end
 		entry do
-			command 'enter or SHIFT+enter'
-			name 'Pressing “enter” will open the currently selected card. Pressing “shift + enter” while submitting a card will open it immediately after creating it.'
+			command 'Enter'
+			name 'Pressing `enter` will open the currently selected card. Pressing `shift + enter` while submitting a card will open it immediately after creating it.'
 		end
 		entry do
-			command 'f'
-			name 'Use “f” to open the card filter menu. The search by title input is automatically focused.'
+			command 'F'
+			name 'Use `F` to open the card filter menu. The search by title input is automatically focused.'
 		end
 		entry do
-			command 'l'
-			name 'Pressing “l” opens a pop-over of the available labels. Clicking a label will add or remove it from the card. Pressing a number key to select a label in that position.'
+			command 'L'
+			name 'Pressing `L` opens a pop-over of the available labels. Clicking a label will add or remove it from the card. Pressing a number key to select a label in that position.'
 		end
 		entry do
-			command 'm'
-			name 'Pressing “m” opens the add / remove members menu. Clicking a member’s avatar will assign or unassign that person.'
+			command 'M'
+			name 'Pressing `M` opens the add / remove members menu. Clicking a member’s avatar will assign or unassign that person.'
 		end
 		entry do
-			command 'n'
-			name 'Pressing “n” opens a pop-over that allows you to add a card after the currently selected card.'
+			command 'N'
+			name 'Pressing `N` opens a pop-over that allows you to add a card after the currently selected card.'
 		end
 		entry do
 			command '<'
@@ -76,50 +76,50 @@ cheatsheet do
 			name 'Pressing the right angle brackets will move a card to the adjacent right list.'
 		end
 		entry do
-			command 'q'
-			name 'Pressing the “q” key toggles the “cards assigned to me” filter.'
+			command 'Q'
+			name 'Pressing the `Q` key toggles the `cards assigned to me` filter.'
 		end
 		entry do
-			command 's'
-			name 'Pressing “s” will subscribe you to, or unsubscribe you from, a card. Subscribing to a card will give you notifications for most actions to that card.'
+			command 'S'
+			name 'Pressing `S` will subscribe you to, or unsubscribe you from, a card. Subscribing to a card will give you notifications for most actions to that card.'
 		end
 		entry do
-			command 'space'
-			name 'Pressing “space” will assign (or unassign) yourself to a card.'
+			command 'Space'
+			name 'Pressing `space` will assign (or unassign) yourself to a card.'
 		end
 		entry do
-			command 't'
-			name 'If viewing a card, pressing “t” will edit the title. If hovering over a card, pressing “t” will open the card and edit the title.'
+			command 'T'
+			name 'If viewing a card, pressing `T` will edit the title. If hovering over a card, pressing `T` will open the card and edit the title.'
 		end
 		entry do
-			command 'v'
-			name 'Pressing “v” will add (or remove) your vote on a card if the Voting Power-Up is enabled.'
+			command 'V'
+			name 'Pressing `V` will add (or remove) your vote on a card if the Voting Power-Up is enabled.'
 		end
 		entry do
-			command 'w'
-			name 'Pressing “w” will collapse or expand the board menu, the sidebar on the right.'
+			command 'W'
+			name 'Pressing `W` will collapse or expand the board menu, the sidebar on the right.'
 		end
 		entry do
-			command 'x'
-			name 'Use “x” to clear all active card filters.'
+			command 'X'
+			name 'Use `X` to clear all active card filters.'
 		end
 		entry do
 			command '?'
-			name 'Pressing “?” will open the shortcuts page.'
+			name 'Pressing `?` will open the shortcuts page.'
 		end
 		entry do
 			command '@'
-			name 'When writing a comment, you can type “@” plus a member’s name, username, or initials and get a list of matching members. You can navigate that list with the up and down arrows. Pressing enter or tab with a member selected will mention that user in the comment. The mentioned user will get a notification once submitted.
+			name 'When writing a comment, you can type `@` plus a member’s name, username, or initials and get a list of matching members. You can navigate that list with the up and down arrows. Pressing enter or tab with a member selected will mention that user in the comment. The mentioned user will get a notification once submitted.
 
 			When adding a new card, you can use the same method to assign members to cards before submitting them.'
 		end
 		entry do
 			command '#'
-			name 'When adding a new card, you can type “#” plus the label’s color or title and get a list of matching labels. You can use the up and down arrows to navigate the resulting list. Pressing enter or tab will add the label to the composed card. The labels will be added to the card when you submit.'
+			name 'When adding a new card, you can type `#` plus the label’s color or title and get a list of matching labels. You can use the up and down arrows to navigate the resulting list. Pressing enter or tab will add the label to the composed card. The labels will be added to the card when you submit.'
 		end
 		entry do
 			command '^'
-			name 'When adding a new card, you can type “^” plus a list name or position in a list. You can also type “top” or “bottom” to add to the top or bottom of the current list. You can use the up and down arrows to navigate the resulting list. Pressing enter or tab will automatically change the position of the composed card.'
+			name 'When adding a new card, you can type `^` plus a list name or position in a list. You can also type `top` or `bottom` to add to the top or bottom of the current list. You can use the up and down arrows to navigate the resulting list. Pressing enter or tab will automatically change the position of the composed card.'
 		end
 	end
 	notes 'Find more details at [trello.com/shortcuts](https://trello.com/shortcuts).'
