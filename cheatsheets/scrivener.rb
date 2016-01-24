8a69d235ea70f8eeb48dc7b0858f79621997ffcc
stbraun <sb@action.ms>
2016-01-24 19:07:08 +0100

    Scrivener cheatsheet added.

---
+++ cheatsheets/scrivener.rb
@@ -0,0 +1,141 @@
+# coding: utf-8
+cheatsheet do
+  title 'Scrivener'
+  docset_file_name 'Scrivener'
+  keyword 'scrivener'
+
+  introduction 'Often used Scrivener keyboard short cuts.'
+
+  category do
+    id 'View'
+    entry do
+      command '⌘ 1'
+      name 'view document'
+    end
+    entry do
+      command '⌘ 2'
+      name 'view corkboard'
+    end
+    entry do
+      command '⌘ 3'
+      name 'view outline'
+    end
+    entry do
+      command '⌥ ⌘ F'
+      name 'toggle mode'
+      notes 'Toggle between normal view and focused view.'
+    end
+    entry do
+      command '⌥ ⌘ I'
+      name 'toggle Inspector'
+    end
+    entry do
+      command '⌥ ⌘ B'
+      name 'toggle Binder'
+    end
+    entry do
+      command '⇧ ⌘ R'
+      name 'hide Format Bar'
+    end
+    entry do
+      command '⇧ ⌥ ⌘ H'
+      name 'toggle Project Keywords'
+    end
+    entry do
+      command '⇧ ⌥ ⌘ P'
+      name 'toggle Page View'
+    end
+    entry do
+      command '⌥ ⌘ P'
+      name 'open Project Notes'
+    end
+    entry do
+      command '⇧ ⌘ Enter'
+      name 'Scratch Pad'
+      notes 'Short cut works also outside Scrivener. So you can quickly take some notes without waiting for Scrivener to start.'
+    end
+  end
+  category do
+    id 'Navigation'
+    entry do
+      command '⌘ 4'
+      name 'open selected item'
+      notes 'open selected item in editor'
+    end
+    entry do
+      command '⌥ ⌘ UpArrow'
+      name 'previous item'
+      notes 'go to previous binder item'
+    end
+    entry do
+      command '⌥ ⌘ DownArrow'
+      name 'next item'
+      notes 'go to next binder item'
+    end
+    entry do
+      command '⌃ ⌘ R'
+      name 'enclosing group'
+      notes 'go to enclosing group'
+    end
+    entry do
+      command '⌥ ⌘ R'
+      name 'reveal text in binder'
+      notes 'Select edited text in binder. Especially handy if binder window is closed.'
+    end
+    entry do
+      command '⌃ Tab'
+      name 'switch focus'
+      notes 'Switch between binder and editor.'
+    end
+    entry do
+      command '⌃ ⌘ T'
+      name 'toggle typewriter scrolling mode'
+    end
+  end
+  category do
+    id 'Searching'
+    entry do
+      command '⌘ F'
+      name 'find'
+    end
+    entry do
+      command '⌘ G'
+      name 'find next'
+    end
+    entry do
+      command '⇧ ⌘ G'
+      name 'find previous'
+    end
+    entry do
+      command '⌘E'
+      name 'use selection for find'
+    end
+  end
+  category do
+    id 'Snapshots'
+    entry do
+      command '⌘ 5'
+      name 'take snapshot'
+    end
+    entry do
+      command '⇧ ⌘ 5'
+      name 'take snapshot with title'
+    end
+  end
+  category do
+    id 'Review'
+    entry do
+      command '⇧ ⌘ *'
+      name 'add comment'
+    end
+    entry do
+      command '⌃ ⌘ 8'
+      name 'add footnote'
+    end
+    entry do
+      command '⇧ ⌘ H'
+      name 'highlight text'
+    end
+  end
+  notes 'Find the manual in *Help/Scrivener Manual* or using *⌘ ?*'
+end
