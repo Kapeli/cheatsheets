7ad27e0a4a7ed4c8a25ca446378eef1c58ee9dd3
Kapeli <support@kapeli.com>
2016-02-01 00:52:16 +0200

    HipChat fixes

--- cheatsheets/HipChat.rb
+++ cheatsheets/HipChat.rb
@@ -1,33 +1,29 @@
 cheatsheet do
-  title 'HipChat Cheatsheet'
-  docset_file_name 'HipChat_Cheatsheet'
+  title 'HipChat'
+  docset_file_name 'HipChat'
   keyword 'hipchat'
-  # resources 'resources_dir'
-  introduction 'HipChat (for Mac) keyboard shortcuts and slash (/) commands'
+  source_url 'http://cheat.kapeli.com'
+
   category do
     id 'Keyboard Shortcuts'
     entry do
-      command 'CMD + /'
+      command 'CMD+/'
       name    'Help'
-      notes   'Open help menu'
     end
     entry do
-      command 'CMD + F'
-      name    'Search'
-      notes   'Search the room history'
+      command 'CMD+F'
+      name   'Search room history'
     end
     entry do
-      command 'CMD + Option + DOWN'
-      name    'Next room'
-      notes   'Next room or 1-1 chat'
+      command 'CMD+ALT+Arrow Down'
+      name   'Next room or 1-1 chat'
     end
     entry do
-      command 'CMD + Option + UP'
-      name    'Previous room'
-      notes   'Previous room or 1-1 chat'
+      command 'CMD+ALT+Arrow Up'
+      name   'Previous room or 1-1 chat'
     end
     entry do
-      command 'CMD + <number>'
+      command 'CMD+<number>'
       name    'Go to room #'
       notes   "
                The Lobby or room at the top is `1`,
@@ -35,37 +31,32 @@ cheatsheet do
               "
     end
     entry do
-      command 'CMD + I'
-      name    'Invite'
-      notes   'Invite someone to a room'
+      command 'CMD+I'
+      name   'Invite someone to a room'
     end
     entry do
-      command 'CMD + Shift + N'
-      name    'Create room'
-      notes   'Create a new room'
+      command 'CMD+SHIFT+N'
+      name   'Create new room'
     end
     entry do
-      command 'CMD + T'
-      name    'Join new room'
-      notes   'Join a new room or 1-1 chat'
+      command 'CMD+T'
+      name   'Join new room or 1-1 chat'
     end
     entry do
-      command 'CMD + Shift + T'
-      name    'Reopen'
+      command 'CMD+SHIFT+T'
       name    'Reopen closed chats'
     end
     entry do
-      command 'CMD + W'
-      name    'Close room'
-      notes   'Close the current room or 1-1 chat'
+      command 'CMD+W'
+      name   'Close current room or 1-1 chat'
     end
   end
   category do
     id 'Formatting'
     entry do
       command '/code code-block'
-      name    'message with syntax highlighting'
-      notes   'Display the message with code syntax highlighting.'
+      name    'Message with syntax highlighting'
+      notes   'Displays your message with code syntax highlighting.'
     end
     entry do
       command '/quote message'
@@ -75,15 +66,12 @@ cheatsheet do
     entry do
       command 's/wrong word/right word'
       name    'Fix typo'
-      notes   'Fix your typos, if corrected within **one** minute'
+      notes   'Fix your typos, if corrected within **one** minute.'
     end
     entry do
       command '/clear'
       name    'Clear history'
-      notes   "
-              Clear chat history in the current room or 1-1 chat.<br />
-              (History comes back when you reopen the room or chat.)
-              "
+      notes   "Clear chat history in the current room or 1-1 chat. History comes back when you reopen the room or chat."
     end
     entry do
       command '/me message'
@@ -92,65 +80,47 @@ cheatsheet do
     end
     entry do
       command '#color-hex'
-      notes   'colors of hex-value'
-      notes   "
-              Show hexadecimal web colors right in HipChat.<br />
-              Example `#ffffff` shows a white box next to #ffffff
-              "
+      name   'Colors of hex-value'
+      notes   "Show hexadecimal web colors right in HipChat. Example: `#ffffff` shows a white box next to `#ffffff`."
     end
   end
   category do
-    id 'Status update /-commands'
+    id 'Status Update Commands'
     entry do
       command '/available message'
       name    'Status "available"'
-      notes   "
-              Set your status as available (green) and optionally includes a message<br />
-              Example `/available Working from home`
-              "
+      notes   "Set your status as available (green) and optionally include a message. Example: `/available Working from home`."
     end
     entry do
       command '/away message'
       name    'Status "away"'
-      notes   "Set your status as away (yellow) and optionally includes a message<br />
-               Example: `/away Out to lunch`
-               "
+      notes   "Set your status as away (yellow) and optionally include a message. Example: `/away Out to lunch`."
     end
     entry do
       command '/dnd message'
       name    'Status "Do Not Disturb"'
-      notes   "Set your status as Do Not Disturb (red) and optionally includes a message<br />
-              Example: `/dnd Finishing proposal`
-              "
+      notes   "Set your status as Do Not Disturb (red) and optionally include a message. Example: `/dnd Finishing proposal`."
     end
   end
   category do
-    id 'Room related /-commands'
+    id 'Room Related Commands'
     entry do
       command '/join room-name'
       name    'Enter room'
-      notes   "
-              (HipChat web app only)
-              Enter an existing HipChat room<br />
-              Example `/join IT Team`
-              "
+      notes   "HipChat web app only: enter an existing HipChat room. Example: `/join IT Team`."
     end
     entry do
       command '/part'
-      name    'Leave room'
-      notes   'Leave the room you are currently in'
+      name    'Leave current room'
     end
     entry do
       command '/topic new-topic'
       name    'Change topic'
-      notes   "
-              Change the topic of the room you are currently in<br />
-              Example: `/topic All things IT`
-              "
+      notes   "Change the topic of the room you are currently in. Example: `/topic All things IT`."
     end
   end
   notes  "
-         * There may be further /-commands depending on you [HipChat Integrations](https://www.hipchat.com/integrations).
+         * There may be further /-commands depending on your [HipChat Integrations](https://www.hipchat.com/integrations).
          * Based on [Atlassian](https://www.atlassian.com/software/hipchat)'s
            [HipChat Documentation](https://confluence.atlassian.com/hipchat/keyboard-shortcuts-and-slash-commands-749385232.html),
          * Created end of January 2016 by [Steffen Clausjuergens](https://twitter.com/stclj).
