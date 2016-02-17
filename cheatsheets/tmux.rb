7c84b2cdaa55120a173b88871ea4b17eb3af7977
Norbert Varga <norbert.varga@supercharge.io>
2016-02-17 11:01:54 +0100

    Entry for zoom toggle

--- cheatsheets/tmux.rb
+++ cheatsheets/tmux.rb
@@ -160,6 +160,11 @@ cheatsheet do
     end
 
     entry do
+      command 'PREFIX-z'
+      name 'Toggle active pane between zoomed and unzoomed'
+    end
+
+    entry do
       command 'PREFIX-"+"'
       name 'Break pane into window (e.g. to select text by mouse to copy)'
     end
@@ -367,25 +372,25 @@ cheatsheet do
 
   category do
     id 'Resizing Panes'
-    
+
     entry do
       notes <<-'END'
         With most of these the number (`##`) is optional and can be omitted to resize one line.
-        
+
         All of these can be bound to keys:
-        
+
         * `bind H resize-pane -L 5`
         * `bind J resize-pane -D 5`
         * `bind K resize-pane -U 5`
         * `bind L resize-pane -R 5`
       END
     end
-    
+
     entry do
       command 'PREFIX-: resize-pane -D ##'
       name 'Resize the current pane down by `##` lines. The `-D` is optional since by default it moves down'
     end
-    
+
     entry do
       command 'PREFIX-: resize-pane -U ##'
       name 'Resize the current pane up `##` lines'
@@ -400,17 +405,17 @@ cheatsheet do
       command 'PREFIX-: resize-pane -R ##'
       name 'Resize the current pane right `##` lines'
     end
-    
+
     entry do
       command 'PREFIX-: resize-pane -y ##'
       name 'Resize the current pane to `##` lines tall'
     end
-    
+
     entry do
       command 'PREFIX-: resize pane -x ##'
       name 'Resize the current pane to `##` lines wide'
     end
-    
+
   end
 
   category do
