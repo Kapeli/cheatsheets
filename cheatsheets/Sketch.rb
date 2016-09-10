45cd56d3e1e26770a1134e40c6a7ac5b1d5a517e
Victor Wang <wjp2013@gmail.com>
2016-09-10 15:20:21 +0800

    Upgrade Sketch Shortcuts

--- cheatsheets/Sketch.rb
+++ cheatsheets/Sketch.rb
@@ -1,297 +1,431 @@
 cheatsheet do
-    title 'Sketch'
-    docset_file_name 'Sketch'
-    keyword 'sketch'
-    source_url 'http://cheat.kapeli.com'
+  title 'Sketch'
+  docset_file_name 'Sketch'
+  keyword 'sketch'
+  source_url 'http://cheat.kapeli.com'
 
-    category do
-        id 'Insert'
+  category do
+    id 'Insert'
 
-        entry do
-            name 'Rectangle'
-            command 'R'
-        end
-
-        entry do
-            name 'Rounded rectangle'
-            command 'U'
-        end
-
-        entry do
-            name 'Oval'
-            command 'O'
-        end
-
-        entry do
-            name 'Vector point'
-            command 'V'
-        end
-
-        entry do
-            name 'Text'
-            command 'T'
-        end
-
-        entry do
-            name 'Line'
-            command 'L'
-        end
+    entry do
+      name 'New Artboard'
+      command 'A'
     end
 
-    category do
-        id 'Canvas View'
-
-        entry do
-            name 'Zoom in'
-            command 'CMD+"+"'
-        end
-
-        entry do
-            name 'Zoom out'
-            command 'CMD+"-"'
-        end
-
-        entry do
-            name 'Actual size'
-            command 'CMD+0'
-        end
-
-        entry do
-            name 'Center canvas'
-            command 'CMD+1'
-        end
-
-        entry do
-            name 'Center selection'
-            command 'CMD+3'
-        end
-
-        entry do
-            name 'Zoom selection'
-            command 'CMD+2'
-        end
-
-        entry do
-            name 'Toggle rulers'
-            command 'CTRL+R'
-        end
-
-        entry do
-            name 'Toggle grid'
-            command 'CTRL+G'
-        end
-
-        entry do
-            name 'Toggle layer guides'
-            command 'CTRL+L'
-        end
-
-        entry do
-            name 'Toggle pixels'
-            command 'CTRL+P'
-        end
-
-        entry do
-            name 'Toggle selection handles'
-            command 'CTRL+H'
-        end
-
-        entry do
-            name 'Toggle pixel grid'
-            command 'CTRL+H'
-        end
+    entry do
+      name 'Slice'
+      command 'S'
     end
 
-    category do
-        id 'Window'
-
-        entry do
-            name 'Toggle layers list'
-            command 'CMD+ALT+1'
-        end
-
-        entry do
-            name 'Toggle inpector'
-            command 'CMD+ALT+2'
-        end
-
-        entry do
-            name 'Toggle layers, inspector'
-            command 'CMD+ALT+3'
-        end
-
-        entry do
-            name 'Toggle toolbar'
-            command 'CMD+ALT+T'
-        end
-
-        entry do
-            name 'Fullscreen'
-            command 'CMD+CTRL+F'
-        end
+    entry do
+      name 'Rectangle'
+      command 'R'
     end
 
-    category do
-        id 'Editing Shapes'
-
-        entry do
-            name 'Use as mask span'
-            command 'CMD+CTRL+M'
-            notes 'Works for layers and groups'
-        end
-
-        entry do
-            name 'Union'
-            command 'CMD+ALT+U'
-        end
-
-        entry do
-            name 'Substract'
-            command 'CMD+ALT+S'
-        end
-
-         entry do
-            name 'Intersect'
-            command 'CMD+ALT+I'
-        end
-
-        entry do
-            name 'Difference'
-            command 'CMD+ALT+X'
-        end
+    entry do
+      name 'Rounded Rectangle'
+      command 'U'
     end
 
-    category do
-        id 'Editing Layers'
-
-        entry do
-            name 'Keep current selection'
-            command 'A'
-        end
-
-        entry do
-            name 'Show distance to other layers'
-            command 'ALT'
-        end
-        
-        entry do
-            name 'Show distance to other layers inside group'
-            command 'CMD+ALT'
-        end
-
-        entry do
-            name 'Duplicate'
-            command 'ALT+Drag'
-            notes 'Repeat with `CMD+D`'
-        end
-
-        entry do
-            name 'Copy style'
-            command 'CMD+ALT+C'
-        end
-
-        entry do
-            name 'Paste style'
-            command 'CMD+ALT+V'
-        end
-
-        entry do
-            name 'Color picker'
-            command 'CTRL+C'
-        end
-
-        entry do
-            name 'Transform'
-            command 'CMD+T'
-        end
-
-        entry do
-            name 'Rotate'
-            command 'CMD+SHIFT+R'
-        end
-
-        entry do
-            name 'Toggle fill'
-            command 'F'
-        end
-
-        entry do
-            name 'Toggle border'
-            command 'B'
-        end
+    entry do
+      name 'Oval'
+      command 'O'
     end
 
-    category do
-        id 'Arranging Layers, Groups and Artboards'
-
-        entry do
-            name 'Bring forward'
-            command 'CMD+ALT+Arrow Up'
-        end
-
-        entry do
-            name 'Bring to front'
-            command 'CMD+CTRL+ALT+Arrow Up'
-        end
-
-        entry do
-            name 'Send backward'
-            command 'CMD+ALT+Arrow Down'
-        end
-
-        entry do
-            name 'Send to back'
-            command 'CMD+CTRL+ALT+Arrow Down'
-        end
-
-        entry do
-            name 'Hide'
-            command 'CMD+SHIFT+H'
-        end
-
-        entry do
-            name 'Lock'
-            command 'CMD+SHIFT+L'
-        end
-
-        entry do
-            name 'Rename'
-            command 'CMD+R'
-        end
-
-        entry do
-            name 'Group layers'
-            command 'CMD+G'
-        end
-
-        entry do
-            name 'Ungroup layers'
-            command 'CMD+SHIFT+G'
-        end
-
-        entry do
-            name 'Select above layer'
-            command 'SHIFT+Tab'
-        end
-
-        entry do
-            name 'Select layer below'
-            command 'Tab'
-        end
-
-        entry do
-            name 'Select parent artboard'
-            command 'Esc'
-        end
-
-        entry do
-            name 'Find layer by name'
-            command 'CMD+F'
-        end
+    entry do
+      name 'Line'
+      command 'L'
     end
+
+    entry do
+      name 'Vector Point'
+      command 'V'
+    end
+
+    entry do
+      name 'Pencil'
+      command 'P'
+    end
+
+    entry do
+      name 'Text'
+      command 'T'
+    end
+  end
+
+  category do
+    id 'Type'
+
+    entry do
+      name 'Bold'
+      command 'CMD + B'
+    end
+
+    entry do
+      name 'Italic'
+      command 'CMD + I'
+    end
+
+    entry do
+      name 'Underline'
+      command 'CMD + U'
+    end
+
+    entry do
+      name 'Increase Font Size'
+      command 'CMD + ALT (+) +'
+    end
+
+    entry do
+      name 'Decrease Font Size'
+      command 'CMD + ALT (+) -'
+    end
+
+    entry do
+      name 'Increase Character Spacing'
+      command 'ALT + CTRL + L'
+    end
+
+    entry do
+      name 'Decrease Character Spacing'
+      command 'ALT + CTRL + T'
+    end
+
+    entry do
+      name 'Change Font'
+      command 'CMD + T'
+    end
+
+    entry do
+      name 'Convert Text to Outlines'
+      command 'CMD + SHIFT + O'
+    end
+
+    entry do
+      name 'Align Left'
+      command 'CMD + SHIFT + {'
+    end
+
+    entry do
+      name 'Align Center'
+      command 'CMD + SHIFT + |'
+    end
+
+    entry do
+      name 'Align Right'
+      command 'CMD + SHIFT + }'
+    end
+
+    entry do
+      name 'Special Characters'
+      command 'CMD + CTRL + SPACE'
+    end
+  end
+
+  category do
+    id 'Canvas View'
+
+    entry do
+      name 'Zoom In'
+      command 'CMD (+) +'
+    end
+
+    entry do
+      name 'Zoom Out'
+      command 'CMD (+) -'
+    end
+
+    entry do
+      name 'Actual Size'
+      command 'CMD + 0'
+    end
+
+    entry do
+      name 'Center Canvas'
+      command 'CMD + 1'
+    end
+
+    entry do
+      name 'Zoom Selection'
+      command 'CMD + 2'
+    end
+
+    entry do
+      name 'Center Selection'
+      command 'CMD + 3'
+    end
+
+    entry do
+      name 'Temporary Zoom to Actual Size'
+      command '§'
+    end
+
+    entry do
+      name 'Focus on First Input Field'
+      command 'ALT + TAB'
+    end
+
+    entry do
+      name 'Toggle Rulers'
+      command 'CTRL + R'
+    end
+
+    entry do
+      name 'Toggle Grid'
+      command 'CTRL + G'
+    end
+
+    entry do
+      name 'Toggle Layer Guides'
+      command 'CTRL + L'
+    end
+
+    entry do
+      name 'Toggle Pixels'
+      command 'CTRL + P'
+    end
+
+    entry do
+      name 'Toggle Selection Handles'
+      command 'CTRL + H'
+    end
+
+    entry do
+      name 'Toggle Pixel Grid'
+      command 'CTRL + X'
+    end
+
+    entry do
+      name 'Move Canvas'
+      command 'SPACE + DRAG'
+    end
+  end
+
+  category do
+    id 'Window'
+
+    entry do
+      name 'Toggle Between Documents'
+      command 'CMD + ~'
+    end
+
+    entry do
+      name 'Toggle Layers List'
+      command 'CMD + ALT + 1'
+    end
+
+    entry do
+      name 'Toggle Inpector'
+      command 'CMD + ALT + 2'
+    end
+
+    entry do
+      name 'Toggle Layers, Inspector'
+      command 'CMD + ALT + 3'
+    end
+
+    entry do
+      name 'Toggle Toolbar'
+      command 'CMD + ALT + T'
+    end
+
+    entry do
+      name 'Presentation Mode'
+      command 'CMD + .'
+    end
+
+    entry do
+      name 'Fullscreen'
+      command 'CMD + CTRL + F'
+    end
+  end
+
+  category do
+    id 'Editing Shapes'
+
+    entry do
+      name 'Keep Current Selection'
+      command 'CMD + ALT'
+    end
+
+    entry do
+      name 'Use as Mask Span'
+      command 'CMD + CTRL + M'
+      notes 'Works for layers and groups'
+    end
+
+    entry do
+      name 'Union'
+      command 'CMD + ALT + U'
+    end
+
+    entry do
+      name 'Substract'
+      command 'CMD + ALT + S'
+    end
+
+    entry do
+      name 'Intersect'
+      command 'CMD + ALT + I'
+    end
+
+    entry do
+      name 'Difference'
+      command 'CMD + ALT + X'
+    end
+
+    entry do
+      name 'Change Object Size'
+      command 'CMD + ARROWS'
+    end
+
+    entry do
+      name 'Change Units by 10'
+      command 'CMD + SHIFT + ARROWS'
+    end
+
+    entry do
+      name 'Change Vector Point Style'
+      command '1, 2, 3, 4'
+    end
+  end
+
+  category do
+    id 'Editing Layers'
+
+    entry do
+      name 'Show Distance to other Layers'
+      command 'ALT'
+    end
+
+    entry do
+      name 'Show Distance to other Layers inside Group'
+      command 'CMD + ALT'
+    end
+
+    entry do
+      name 'Duplicate'
+      command 'ALT + DRAG'
+      notes 'Repeat with `CMD + D`'
+    end
+
+    entry do
+      name 'Copy Style'
+      command 'CMD + ALT + C'
+    end
+
+    entry do
+      name 'Paste Style'
+      command 'CMD + ALT + V'
+    end
+
+    entry do
+      name 'Color Picker'
+      command 'CTRL + C'
+    end
+
+    entry do
+      name 'Transform'
+      command 'CMD + T'
+    end
+
+    entry do
+      name 'Rotate'
+      command 'CMD + SHIFT + R'
+    end
+
+    entry do
+      name 'Toggle Fill'
+      command 'F'
+    end
+
+    entry do
+      name 'Toggle Border'
+      command 'B'
+    end
+  end
+
+  category do
+    id 'Arranging Layers, Groups and Artboards'
+
+    entry do
+      name 'Bring Forward'
+      command 'CMD + ALT + ARROW UP'
+    end
+
+    entry do
+      name 'Bring to Front'
+      command 'CMD + CTRL + ALT + ARROW UP'
+    end
+
+    entry do
+      name 'Send Backward'
+      command 'CMD + ALT + ARROW DOWN'
+    end
+
+    entry do
+      name 'Send to Back'
+      command 'CMD + CTRL + ALT + ARROW DOWN'
+    end
+
+    entry do
+      name 'Hide'
+      command 'CMD + SHIFT + H'
+    end
+
+    entry do
+      name 'Lock'
+      command 'CMD + SHIFT + L'
+    end
+
+    entry do
+      name 'Rename'
+      command 'CMD + R'
+    end
+
+    entry do
+      name 'Group Layers'
+      command 'CMD + G'
+    end
+
+    entry do
+      name 'Ungroup Layers'
+      command 'CMD + SHIFT + G'
+    end
+
+    entry do
+      name 'Select Above Layer'
+      command 'SHIFT + Tab'
+    end
+
+    entry do
+      name 'Select Layer Below'
+      command 'Tab'
+    end
+
+    entry do
+      name 'Select Parent Artboard'
+      command 'Esc'
+    end
+
+    entry do
+      name 'Find Layer by Name'
+      command 'CMD + F'
+    end
+
+    entry do
+      name 'Select Above Page'
+      command 'Fn + ARROW UP'
+    end
+
+    entry do
+      name 'Select Page Below'
+      command 'Fn + ARROW DOWN'
+    end
+  end
 
   notes <<-'END'
     * Based on [sketchshortcuts.com](http://sketchshortcuts.com) by [Robert van Klinken](https://twitter.com/robertvklinken) & [Jeffrey de Groot](https://twitter.com/jeffreydgroot).
-    * Converted by [Frank Betorina](https://twitter.com/frankbetorina).
+    * Converted by [Victor Wang](http://wjp2013.github.io/).
   END
 end
