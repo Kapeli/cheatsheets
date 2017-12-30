ca2a5492ca582ecace370043b23ed020468e6118
Andy Waite <andyw8@users.noreply.github.com>
2017-12-30 13:27:54 -0500

    Remove invalid delete commands
    In tmux's copy mode, there's no way to delete content. I suspect these
    commands may have been originally copied from a general Vim cheatsheet.
    
    cc @jongold who first added this.

--- cheatsheets/tmux.rb
+++ cheatsheets/tmux.rb
@@ -273,16 +273,6 @@ cheatsheet do
     end
 
     entry do
-      name 'Delete entire line'
-      command 'd'
-    end
-
-    entry do
-      name 'Delete to end of line'
-       command 'D'
-    end
-
-    entry do
       name 'End of line'
       command '$'
     end
