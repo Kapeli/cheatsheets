b35efb7f4ceaf2a81058f986c91eb3d26c2eafd8
Thomas Cazade <cazade.thomas@gmail.com>
2018-02-10 16:17:49 +0100

    Request config, defaults configs

--- cheatsheets/Axios.rb
+++ cheatsheets/Axios.rb
@@ -162,4 +162,239 @@ cheatsheet do
       END
     end
   end
+  
+  category do
+    id 'Request config'
+    
+    entry do
+      name 'Config options for requests'
+      notes <<-'END'
+        ```javascript
+        {
+          // `url` is the server URL that will be used for the request
+          url: '/user',
+
+          // `method` is the request method to be used when making the request
+          method: 'get', // default
+
+          // `baseURL` will be prepended to `url` unless `url` is absolute.
+          // It can be convenient to set `baseURL` for an instance of axios to pass relative URLs
+          // to methods of that instance.
+          baseURL: 'https://some-domain.com/api/',
+
+          // `transformRequest` allows changes to the request data before it is sent to the server
+          // This is only applicable for request methods 'PUT', 'POST', and 'PATCH'
+          // The last function in the array must return a string or an instance of Buffer, ArrayBuffer,
+          // FormData or Stream
+          // You may modify the headers object.
+          transformRequest: [function (data, headers) {
+            // Do whatever you want to transform the data
+
+            return data;
+          }],
+
+          // `transformResponse` allows changes to the response data to be made before
+          // it is passed to then/catch
+          transformResponse: [function (data) {
+            // Do whatever you want to transform the data
+
+            return data;
+          }],
+
+          // `headers` are custom headers to be sent
+          headers: {'X-Requested-With': 'XMLHttpRequest'},
+
+          // `params` are the URL parameters to be sent with the request
+          // Must be a plain object or a URLSearchParams object
+          params: {
+            ID: 12345
+          },
+
+          // `paramsSerializer` is an optional function in charge of serializing `params`
+          // (e.g. https://www.npmjs.com/package/qs, http://api.jquery.com/jquery.param/)
+          paramsSerializer: function(params) {
+            return Qs.stringify(params, {arrayFormat: 'brackets'})
+          },
+
+          // `data` is the data to be sent as the request body
+          // Only applicable for request methods 'PUT', 'POST', and 'PATCH'
+          // When no `transformRequest` is set, must be of one of the following types:
+          // - string, plain object, ArrayBuffer, ArrayBufferView, URLSearchParams
+          // - Browser only: FormData, File, Blob
+          // - Node only: Stream, Buffer
+          data: {
+            firstName: 'Fred'
+          },
+
+          // `timeout` specifies the number of milliseconds before the request times out.
+          // If the request takes longer than `timeout`, the request will be aborted.
+          timeout: 1000,
+
+          // `withCredentials` indicates whether or not cross-site Access-Control requests
+          // should be made using credentials
+          withCredentials: false, // default
+
+          // `adapter` allows custom handling of requests which makes testing easier.
+          // Return a promise and supply a valid response (see lib/adapters/README.md).
+          adapter: function (config) {
+            /* ... */
+          },
+
+          // `auth` indicates that HTTP Basic auth should be used, and supplies credentials.
+          // This will set an `Authorization` header, overwriting any existing
+          // `Authorization` custom headers you have set using `headers`.
+          auth: {
+            username: 'janedoe',
+            password: 's00pers3cret'
+          },
+
+          // `responseType` indicates the type of data that the server will respond with
+          // options are 'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'
+          responseType: 'json', // default
+
+          // `xsrfCookieName` is the name of the cookie to use as a value for xsrf token
+          xsrfCookieName: 'XSRF-TOKEN', // default
+
+          // `xsrfHeaderName` is the name of the http header that carries the xsrf token value
+          xsrfHeaderName: 'X-XSRF-TOKEN', // default
+
+          // `onUploadProgress` allows handling of progress events for uploads
+          onUploadProgress: function (progressEvent) {
+            // Do whatever you want with the native progress event
+          },
+
+          // `onDownloadProgress` allows handling of progress events for downloads
+          onDownloadProgress: function (progressEvent) {
+            // Do whatever you want with the native progress event
+          },
+
+          // `maxContentLength` defines the max size of the http response content allowed
+          maxContentLength: 2000,
+
+          // `validateStatus` defines whether to resolve or reject the promise for a given
+          // HTTP response status code. If `validateStatus` returns `true` (or is set to `null`
+          // or `undefined`), the promise will be resolved; otherwise, the promise will be
+          // rejected.
+          validateStatus: function (status) {
+            return status >= 200 && status < 300; // default
+          },
+
+          // `maxRedirects` defines the maximum number of redirects to follow in node.js.
+          // If set to 0, no redirects will be followed.
+          maxRedirects: 5, // default
+
+          // `httpAgent` and `httpsAgent` define a custom agent to be used when performing http
+          // and https requests, respectively, in node.js. This allows options to be added like
+          // `keepAlive` that are not enabled by default.
+          httpAgent: new http.Agent({ keepAlive: true }),
+          httpsAgent: new https.Agent({ keepAlive: true }),
+
+          // 'proxy' defines the hostname and port of the proxy server
+          // Use `false` to disable proxies, ignoring environment variables.
+          // `auth` indicates that HTTP Basic auth should be used to connect to the proxy, and
+          // supplies credentials.
+          // This will set an `Proxy-Authorization` header, overwriting any existing
+          // `Proxy-Authorization` custom headers you have set using `headers`.
+          proxy: {
+            host: '127.0.0.1',
+            port: 9000,
+            auth: {
+              username: 'mikeymike',
+              password: 'rapunz3l'
+            }
+          },
+
+          // `cancelToken` specifies a cancel token that can be used to cancel the request
+          // (see Cancellation section below for details)
+          cancelToken: new CancelToken(function (cancel) {
+          })
+        }
+        ```
+      END
+    end
+  end
+  
+  category do
+    id 'Response schema'
+    
+    entry do
+      name 'Response for a request'
+      notes <<-'END'
+        ```javascript
+        {
+          // `data` is the response that was provided by the server
+          data: {},
+
+          // `status` is the HTTP status code from the server response
+          status: 200,
+
+          // `statusText` is the HTTP status message from the server response
+          statusText: 'OK',
+
+          // `headers` the headers that the server responded with
+          // All header names are lower cased
+          headers: {},
+
+          // `config` is the config that was provided to `axios` for the request
+          config: {},
+
+          // `request` is the request that generated this response
+          // It is the last ClientRequest instance in node.js (in redirects)
+          // and an XMLHttpRequest instance the browser
+          request: {}
+        }
+        ```
+      END
+    end
+  end
+  
+  category do
+    id 'Config defaults'
+    
+    entry do
+      name 'Global axios defaults'
+      notes <<-'END'
+        ```javascript
+        axios.defaults.baseURL = 'https://api.example.com';
+        axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+        axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+        ```
+      END
+    end
+    
+    entry do
+      name 'Custom instance defaults'
+      notes <<-'END'
+        ```javascript
+        // Set config defaults when creating the instance
+        var instance = axios.create({
+          baseURL: 'https://api.example.com'
+        });
+
+        // Alter defaults after instance has been created
+        instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+        ```
+      END
+    end
+    
+    entry do
+      name 'Config order of precedence'
+      notes <<-'END'
+        ```javascript
+        // Create an instance using the config defaults provided by the library
+        // At this point the timeout config value is `0` as is the default for the library
+        var instance = axios.create();
+
+        // Override timeout default for the library
+        // Now all requests will wait 2.5 seconds before timing out
+        instance.defaults.timeout = 2500;
+
+        // Override timeout for this request as it's known to take a long time
+        instance.get('/longRequest', {
+          timeout: 5000
+        });
+        ```
+      END
+    end
+  end
 end
