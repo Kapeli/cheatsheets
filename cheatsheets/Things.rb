06cbbdfdc6e769aa0eb37847cf3cafdcf7188c5a
Oliver Steele <steele@osteele.com>
2018-10-03 15:22:29 -0400

    Update Things to comply with keyboard shortcut guidelines

--- cheatsheets/Things.rb
+++ cheatsheets/Things.rb
@@ -9,7 +9,7 @@ cheatsheet do
     id 'Create new items'
 
     entry do
-      command '⌘ Cmd + N'
+      command 'CMD+N'
       name 'Create a new to-do'
     end
     entry do
@@ -17,27 +17,27 @@ cheatsheet do
       name 'Create a new to-do below selection'
     end
     entry do
-      command '⌘ Cmd + V '
+      command 'CMD+V'
       name 'Paste plain text to create new to-dos'
     end
     entry do
-      command '⌘ Cmd + ⇧ Shift + C'
+      command 'CMD+SHIFT+C'
       name 'Create a checklist in an open to-do'
     end
     entry do
-      command '⌘ Cmd + ⇧ Shift + N'
+      command 'CMD+SHIFT+N'
       name 'Create a new heading in a project'
     end
     entry do
-      command '⌘ Cmd + ⌥ Opt + N'
+      command 'CMD+ALT+N'
       name 'Create a new project'
     end
     entry do
-      command 'Ctrl + Space'
+      command 'CTRL+Space'
       name 'Open Quick Entry'
     end
     entry do
-      command '⌥ Opt + Ctrl + Space'
+      command 'CTRL+ALT+Space'
       name 'Open Quick Entry with Autofill'
     end
   end
@@ -45,32 +45,32 @@ cheatsheet do
   category do
     id 'Edit items'
     entry do
-      command '↩ Return'
+      command 'Return'
       name 'Open a to-do or project'
     end
     entry do
-      command '⌘ Cmd + ↩ Return'
+      command 'CMD+Return'
       command 'Esc'
       name 'Save and close'
     end
     entry do
-      command '⌘ Cmd + D'
+      command 'CMD+D'
       name 'Duplicate a to-do or project'
     end
     entry do
-      command '⌘ Cmd + C'
+      command 'CMD+C'
       name 'Copy a to-do or project'
     end
     entry do
-      command '⌘ Cmd + V'
+      command 'CMD+V'
       name 'Paste a to-do or project'
     end
     entry do
-      command '⌘ Cmd + .'
+      command 'CMD+.'
       name 'Complete selected items'
     end
     entry do
-      command '⌘ Cmd + ⌥ Opt + .'
+      command 'CMD+ALT+.'
       name 'Cancel selected items'
     end
     entry do
@@ -78,7 +78,7 @@ cheatsheet do
       name 'Delete selected items'
     end
     entry do
-      command '⌘ Cmd + L'
+      command 'CMD+L'
       name 'Move completed to Logbook'
     end
   end
@@ -87,31 +87,31 @@ cheatsheet do
     id 'Select items'
 
     entry do
-      command '↑'
+      command 'Arrow Up'
       name 'Select item above'
     end
     entry do
-      command '↓'
+      command 'Arrow Down'
       name 'Select item below'
     end
     entry do
-      command '⇧ Shift + ↑'
+      command 'SHIFT+Arrow Up'
       name 'Extend selection upwards'
     end
     entry do
-      command '⇧ Shift + ↓'
+      command 'SHIFT+Arrow Down'
       name 'Extend selection downwards'
     end
     entry do
-      command '⌥ Opt + ⇧ Shift + ↑'
+      command 'ALT+SHIFT+Arrow Up'
       name 'Extend selection to the top'
     end
     entry do
-      command '⌥ Opt + ⇧ Shift + ↓'
+      command 'ALT+SHIFT+Arrow Down'
       name 'Extend selection to the bottom'
     end
     entry do
-      command '⌘ Cmd + A'
+      command 'CMD+A'
       name 'Select everything'
     end
   end
@@ -120,28 +120,28 @@ cheatsheet do
     id 'Move items'
 
     entry do
-      command '⌘ Cmd + ⇧ Shift + M	'
+      command 'CMD+SHIFT+M	'
       name 'Move selection to another list'
     end
     entry do
-      command '⌘ Cmd + ⌥ Opt + V	'
+      command 'CMD+ALT+V	'
       name 'Move copied to-dos and projects'
     end
     entry do
-      command '⌘ Cmd + ↑	'
+      command 'CMD+Arrow Up	'
       name 'Move selection up'
     end
     entry do
-      command '⌘ Cmd + ↓	'
+      command 'CMD+Arrow Down	'
       name 'Move selection down'
     end
     entry do
-      command '⌘ Cmd + ⌥ Opt + ↑	'
+      command 'CMD+ALT+Arrow Up	'
       name 'Move selection to top of list'
     end
     entry do
-      command '⌘ Cmd + ⌥ Opt + ↓	'
-      name 'Move selection to bottom of list    '
+      command 'CMD+ALT+Arrow Down	'
+      name 'Move selection to bottom of list'
     end
   end
 
@@ -149,63 +149,63 @@ cheatsheet do
     id 'Edit dates'
 
     entry do
-      command '⌘ Cmd + S'
+      command 'CMD+S'
       name 'Show Jump Start'
     end
     entry do
-      command '⌘ Cmd + T'
+      command 'CMD+T'
       name 'Start Today'
     end
     entry do
-      command '⌘ Cmd + E'
+      command 'CMD+E'
       name 'Start This Evening'
     end
     entry do
-      command '⌘ Cmd + R'
+      command 'CMD+R'
       name 'Start Anytime'
     end
     entry do
-      command '⌘ Cmd + O'
+      command 'CMD+O'
       name 'Start Someday'
     end
     entry do
-      command 'Ctrl + ]'
+      command 'CTRL+]'
       name 'Start date +1 day'
     end
     entry do
-      command 'Ctrl + ['
+      command 'CTRL+['
       name 'Start date -1 day'
     end
     entry do
-      command 'Ctrl + ⇧ Shift + ]'
+      command 'CTRL+SHIFT+]'
       name 'Start date + 1 week'
     end
     entry do
-      command 'Ctrl + ⇧ Shift + ['
+      command 'CTRL+SHIFT+['
       name 'Start date - 1 week'
     end
     entry do
-      command '⌘ Cmd + ⇧ Shift + D'
+      command 'CMD+SHIFT+D'
       name 'Direct access to set a Deadline'
     end
     entry do
-      command 'Ctrl + .'
+      command 'CTRL+.'
       name 'Deadline +1 day'
     end
     entry do
-      command 'Ctrl + ,'
+      command 'CTRL+,'
       name 'Deadline -1 day'
     end
     entry do
-      command 'Ctrl + ⇧ Shift + >'
+      command 'CTRL+SHIFT+>'
       name 'Deadline +1 week'
     end
     entry do
-      command 'Ctrl + ⇧ Shift + <'
+      command 'CTRL+SHIFT+<'
       name 'Deadline -1 week'
     end
     entry do
-      command '⌘ Cmd + ⇧ Shift + R'
+      command 'CMD+SHIFT+R'
       name 'Make to-do or project repeating'
     end
   end
@@ -214,32 +214,32 @@ cheatsheet do
     id 'Control windows'
 
     entry do
-      command '⌘ Cmd + Ctrl + N'
+      command 'CMD+CTRL+N'
       name 'Open new window'
     end
     entry do
-      command '⌘ Cmd + ⇧ Shift + `'
+      command 'CMD+SHIFT+`'
       name 'Cycle through open windows'
     end
     entry do
-      command '⌘ Cmd + W'
+      command 'CMD+W'
       name 'Close current window'
     end
     entry do
-      command '⌘ Cmd + ⌥ Opt + W'
+      command 'CMD+ALT+W'
       name 'Close all windows'
     end
     entry do
-      command '⌘ Cmd + /'
+      command 'CMD+/'
       name 'Hide or show sidebar'
     end
     entry do
-      command '⌘ Cmd + ⌥ Opt + T'
+      command 'CMD+ALT+T'
       name 'Hide or show toolbar'
     end
     entry do
-      command '⌘ Cmd + Ctrl + F'
-      name 'Full screen    '
+      command 'CMD+CTRL+F'
+      name 'Full screen'
     end
   end
 
@@ -247,31 +247,31 @@ cheatsheet do
     id 'Navigate'
 
     entry do
-      command '⌘ Cmd + 1'
+      command 'CMD+1'
       name 'Go to Inbox'
     end
     entry do
-      command '⌘ Cmd + 2'
+      command 'CMD+2'
       name 'Go to Today'
     end
     entry do
-      command '⌘ Cmd + 3'
+      command 'CMD+3'
       name 'Go to Upcoming'
     end
     entry do
-      command '⌘ Cmd + 4'
+      command 'CMD+4'
       name 'Go to Anytime'
     end
     entry do
-      command '⌘ Cmd + 5'
+      command 'CMD+5'
       name 'Go to Someday'
     end
     entry do
-      command '⌘ Cmd + 6'
+      command 'CMD+6'
       name 'Go to Logbook'
     end
     entry do
-      command '⌘ Cmd + ⇧ Shift + L'
+      command 'CMD+SHIFT+L'
       name 'Show to-do in list'
     end
     entry do
@@ -283,20 +283,20 @@ cheatsheet do
       name 'Return to previous list'
     end
     entry do
-      command '⌥ Opt + ↑'
+      command 'ALT+Arrow Up'
       name 'Scroll to top'
     end
     entry do
-      command '⌥ Opt + ↓'
+      command 'ALT+Arrow Down'
       name 'Scroll to bottom'
     end
     entry do
-      command '⌘ Cmd + ⌥ Opt + Ctrl + ↑'
+      command 'CMD+CTRL+ALT+Arrow Up'
       name 'Navigate up in the sidebar'
     end
     entry do
-      command '⌘ Cmd + ⌥ Opt + Ctrl + ↓'
-      name 'Navigate down in the sidebar    '
+      command 'CMD+CTRL+ALT+Arrow Down'
+      name 'Navigate down in the sidebar'
     end
   end
 
@@ -308,7 +308,7 @@ cheatsheet do
       name 'Start typing to begin a search'
     end
     entry do
-      command '⌘ Cmd + F'
+      command 'CMD+F'
       name 'Find'
     end
   end
@@ -317,7 +317,7 @@ cheatsheet do
     id 'Tags'
 
     entry do
-      command '⌘ Cmd + Ctrl + T'
+      command 'CMD+CTRL+T'
       name 'Open tag window'
     end
     entry do
@@ -325,24 +325,24 @@ cheatsheet do
       name 'Close tag window'
     end
     entry do
-      command '⌘ Cmd + ⇧ Shift + T'
+      command 'CMD+SHIFT+T'
       name 'Direct access to add a tag'
     end
     entry do
-      command 'Ctrl + shortcut'
+      command 'CTRL+shortcut'
       name 'Toggle a tag for selected to-do'
     end
     entry do
-      command '⌥ Opt + Ctrl + shortcut'
+      command 'CTRL+ALT+shortcut'
       name 'Filter for a tag'
     end
     entry do
-      command '⌘ Cmd + click any tag'
+      command 'CMD+click any tag'
       name 'Select multiple tags'
     end
     entry do
-      command 'Ctrl + Esc'
-      name 'Destroy previous filter  '
+      command 'CTRL+Esc'
+      name 'Destroy previous filter'
     end
   end
 
@@ -350,7 +350,7 @@ cheatsheet do
     id 'Links'
 
     entry do
-      command '⌘ Cmd + ⌥ Opt + ↩ Return'
+      command 'CMD+ALT+Return'
       name 'Open a link'
     end
   end
