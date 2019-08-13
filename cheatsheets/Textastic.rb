6d36e7bf4ec01da85e5ec841ed01971a11241f20
Brian Reyman <brian@brianreyman.com>
2019-08-13 17:59:27 -0600

    Add Textastic Cheatsheet
    Add keyboard shortcut cheatsheet for Textastic for iOS.

---
+++ cheatsheets/Textastic.rb
@@ -0,0 +1,129 @@
+cheatsheet do
+  title 'Textastic'
+  docset_file_name 'Textastic'
+  keyword 'textastic'
+  source_url 'http://cheat.kapeli.com'
+  introduction 'Keyboard shortcuts for **Textastic**, a robust iOS text editor.'
+
+  category do
+    id 'General'
+    entry do
+      command 'CMD+ALT+B'
+      name 'Toggle sidebar'
+    end
+    entry do
+      command 'CMD+ALT+P'
+      name 'Toggle preview'
+    end
+    entry do
+      command 'CMD+CTRL+ALT+P'
+      name 'Preview in Safari'
+    end
+    entry do
+      command 'CMD+ALT+T'
+      name 'Toggle symbol list'
+    end
+  end
+
+  category do
+    id 'Files'
+    entry do
+      command 'CMD+N'
+      name 'New file/folder'
+    end
+    entry do
+      command 'CMD+E'
+      name 'Edit file'
+    end
+    entry do
+      command 'CMD+F'
+      name 'Toggle find/replace'
+    end
+    entry do
+      command 'CMD+ALT+Arrow Up'
+      name 'Upload file'
+    end
+    entry do
+      command 'CMD+ALT+Arrow Down'
+      name 'Download file'
+    end
+    entry do
+      command 'CMD+ALT+I'
+      name 'Toggle file properties'
+    end
+  end
+
+  category do
+    id 'Code Editor'
+    entry do
+      command 'CMD+['
+      name 'Shift left'
+    end
+    entry do
+      command 'CMD+]'
+      name 'Shift right'
+    end
+    entry do
+      command 'CMD+CTRL+E'
+      name 'Expand Emmet abbreviation'
+    end
+    entry do
+      command 'CMD+CTRL+A'
+      name 'Wrap with Emmet abbreviation'
+    end
+  end
+
+  category do
+    id 'Tabs'
+    entry do
+      command 'CTRL+Tab'
+      name 'Select next tab'
+    end
+    entry do
+      command 'CTRL+SHIFT+Tab'
+      name 'Select previous tab'
+    end
+    entry do
+      command 'CMD+1'
+      name 'Show first tab'
+    end
+    entry do
+      command 'CMD+2'
+      name 'Show second tab'
+    end
+    entry do
+      command 'CMD+3'
+      name 'Show third tab'
+    end
+    entry do
+      command 'CMD+4'
+      name 'Show fourth tab'
+    end
+    entry do
+      command 'CMD+5'
+      name 'Show fifth tab'
+    end
+    entry do
+      command 'CMD+6'
+      name 'Show sixth tab'
+    end
+    entry do
+      command 'CMD+7'
+      name 'Show seventh tab'
+    end
+    entry do
+      command 'CMD+8'
+      name 'Show eigth tab'
+    end
+    entry do
+      command 'CMD+9'
+      name 'Show ninth tab'
+    end
+    entry do
+      command 'CMD+0'
+      name 'Show tenth tab'
+    end
+  end
+
+  notes 'For more on how to use Textastic go to the [Textastic for iOS Manual](https://www.textasticapp.com/v8/manual/).'
+end
